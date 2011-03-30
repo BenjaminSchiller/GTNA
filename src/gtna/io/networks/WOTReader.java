cdb6ade01a8a4609a9a90c56fe89a7b99a971589
Zoran Zaric <zz@zoranzaric.de>
2011-03-30 19:25:25 +0200

    Implement WOTReader

--- src/gtna/io/networks/WOTReader.java
+++ src/gtna/io/networks/WOTReader.java
@@ -1,16 +1,110 @@
 package gtna.io.networks;
 
+import gtna.graph.Edges;
 import gtna.graph.Graph;
 import gtna.graph.NodeImpl;
-import gtna.util.Timer;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+import gtna.util.Timer;
+import gtna.util.Util;
+import java.util.Vector;
+
+/**
+ * This is a reader for the Web of Trust file format version 0.2.
+ * 
+ * The Web of Trust file format specification is available at:
+ *   http://www.lysator.liu.se/~jc/wotsap/wotfileformat.txt
+ * 
+ * *.wot files are compressed with bzip2, this reader assumes a uncompressed
+ * version of the file.
+ * 
+ * @author Zoran Zaric <zz@zoranzaric.de>
+ *
+ */
 public class WOTReader {
+	private final static int SIGNATURE_TYPE_MASK  = 0xF0000000;
+	private final static int SIGNATURE_INDEX_MASK = 0x0FFFFFFF;
+	
 	public static Graph read(String filename) throws Exception {
 		Timer timer = new Timer();
 		
+		RandomAccessFile file = new RandomAccessFile(new File(filename), "r");
+		
+		// seek to names
+		while (!file.readLine().startsWith("names/")) {
+		}
+		
+		// read names
+		Vector<WOTNode> wotNodes = new Vector<WOTNode>();
+		int index = 0;
+		String line;
+		while (!(line = file.readLine()).startsWith("keys/")) {
+			if (!line.equals("")) {
+				WOTNode node = new WOTNode(index, line);
+				wotNodes.add(node);
+				index += 1;
+			}
+		}
+		System.out.println(wotNodes.size() + " Nodes");
+		
+		// read keys
+		for (index = 0; index < wotNodes.size(); index++) {
+			byte[] key = new byte[4];
+			file.read(key);
+			WOTNode node = wotNodes.get(index);
+			node.key = Util.toInt(key);
+		}
+		
+		// skip the "signatures/" line
+		String signaturesLine = file.readLine();
+		assert signaturesLine.startsWith("signatures/");
+		
+		// read signatures
+		for (index = 0; index < wotNodes.size(); index++) {
+			WOTNode node = wotNodes.get(index);
+			
+			// read how many keys are signed by this key
+			byte[] buffer = new byte[4];
+			file.read(buffer);
+			int signatureCount = Util.toInt(buffer);
+			
+			// inspect all signatures
+			for (int signatureIndex = 0; signatureIndex < signatureCount; signatureIndex++) {
+				byte[] signatureBuffer = new byte[4];
+				file.read(signatureBuffer);
+				
+				int signature = Util.toInt(signatureBuffer);
+				
+				// get the signature type
+				// TODO do something with the signature type
+				int signatureType = signature & SIGNATURE_TYPE_MASK;
+				
+				// get the target key
+				int targetIndex = signature & SIGNATURE_INDEX_MASK;
+				assert targetIndex < wotNodes.size();
+				
+				// retrieve the target node
+				WOTNode target = wotNodes.get(targetIndex);
+				node.signatures.add(target);
+			}
+		}
+		// skip the "debug/" line
+		String debugLine = file.readLine();
+		assert debugLine.startsWith("debug/");
+		
+		file.close();
 		timer.end();
 
-		NodeImpl[] nodes = {};
+		// prepare return graph
+		NodeImpl[] nodes = NodeImpl.init(wotNodes.size());
+		Edges edges = new Edges(nodes, nodes.length);
+		for (WOTNode source : wotNodes) {
+			for (WOTNode target : source.signatures) {
+				edges.add(nodes[source.index], nodes[target.index]);
+			}
+		}
+		edges.fill();
 		Graph g = new Graph("NAME", nodes, timer);
 		return g;
 	}
@@ -22,13 +116,13 @@ public class WOTReader {
 
 		public int key;
 
-		public WOTNode[] signatures;
+		public Vector<WOTNode> signatures;
 
 		public WOTNode(int index, String name) {
 			this.index = index;
 			this.name = name;
 			this.key = -1;
-			this.signatures = null;
+			this.signatures = new Vector<WOTReader.WOTNode>();
 		}
 
 		public String toString() {
