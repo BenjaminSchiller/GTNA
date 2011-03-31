9ac382bb680f261857a3d3f2469833979cd6ac95
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2011-03-31 14:26:18 +0200

    different node orders for the Communities3 network generator
    The parameter order is added to the constructor.
    
    order and bidirectional are now part of the configuration parameters.
    
    The parameter "order" can be used to change the position of the nodes in the
    main node array of the generated graph object. If it is set to SORTED_ORDER,
    the main array contains first all nodes from the first community, then the
    nodes from the second community, and so on. In case the flag is true, the
    first node is assigned to the first community, the second node to the second
    and so on. If the communities do not have the same size, the smaller
    communities are simply discarded after they received all their nodes. E.g.,
    sizes = {2, 3} would lead to the communities with node indices {0, 2} {1, 3,
    4}. sizes = {3, 1, 3} would result in the node assignments {0, 3, 5} {1} {2,
    4, 6}. If order is set to RANDOM_ORDER, the nodes are assigned to the
    communities at random.
    
    Also, a getter for the last generated community structure is now included.

--- src/gtna/networks/model/Communities3.java
+++ src/gtna/networks/model/Communities3.java
@@ -10,6 +10,7 @@ import gtna.transformation.Transformation;
 import gtna.util.Timer;
 import gtna.util.Util;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -26,15 +27,17 @@ import java.util.Random;
  * The flag LESS_MEMORY can be set to choose a generation procedure that uses
  * less memory but requires more computation time.
  * 
- * The flag ALTERNATE_NODES can be used to change the position of the nodes in
- * the main node array of the generated graph object. If it is set false, the
- * main array contains first all nodes from the first community, then the nodes
- * from the second community, and so on. In case the flag is true, the first
- * node is assigned to the first community, the second node to the second and so
- * on. If the communities do not have the same size, the smaller communities are
- * simply discarded after they received all their nodes. E.g., sizes = {2, 3}
- * would lead to the communities with node indices {0, 2} {1, 3, 4}. sizes = {3,
- * 1, 3} would result in the node assignments {0, 3, 5} {1} {2, 4, 6}.
+ * The parameter "order" can be used to change the position of the nodes in the
+ * main node array of the generated graph object. If it is set to SORTED_ORDER,
+ * the main array contains first all nodes from the first community, then the
+ * nodes from the second community, and so on. In case the flag is true, the
+ * first node is assigned to the first community, the second node to the second
+ * and so on. If the communities do not have the same size, the smaller
+ * communities are simply discarded after they received all their nodes. E.g.,
+ * sizes = {2, 3} would lead to the communities with node indices {0, 2} {1, 3,
+ * 4}. sizes = {3, 1, 3} would result in the node assignments {0, 3, 5} {1} {2,
+ * 4, 6}. If order is set to RANDOM_ORDER, the nodes are assigned to the
+ * communities at random.
  * 
  * @author benni
  * 
@@ -44,68 +47,82 @@ public class Communities3 extends NetworkImpl implements Network {
 
 	private double[][] p;
 
+	private int order;
+
 	private boolean bidirectional;
+
+	private NodeImpl[][] communities;
 
 	public static final boolean LESS_MEMORY = false;
 
-	public static final boolean ALTERNATE_NODES = true;
+	public static final int SORTED_ORDER = 1;
 
-	public Communities3(int[] sizes, double[][] p, boolean bidirectional,
-			RoutingAlgorithm r, Transformation[] t) {
+	public static final int ALTERNATING_ORDER = 2;
+
+	public static final int RANDOM_ORDER = 3;
+
+	public Communities3(int[] sizes, double[][] p, int order,
+			boolean bidirectional, RoutingAlgorithm r, Transformation[] t) {
 		super("COMMUNITIES3", Util.sum(sizes), new String[] {
-				"COMMUNITY_SIZES", "INTER_COMMUNITY_LINKS" }, new String[] {
-				Util.toFolderString(sizes), Util.toFolderString(p) }, r, t);
+				"COMMUNITY_SIZES", "INTER_COMMUNITY_LINKS", "ORDER",
+				"BIDIRECTIONAL" }, new String[] { Util.toFolderString(sizes),
+				Util.toFolderString(p), "" + order, "" + bidirectional }, r, t);
 		this.sizes = sizes;
 		this.p = p;
+		this.order = order;
 		this.bidirectional = bidirectional;
 	}
-	
-	public int[] getSizes(){
-		return this.sizes;
-	}
-	
-	public double[][] getP(){
-		return this.p;
-	}
-	
-	public boolean isBidirectional(){
-		return this.bidirectional;
+
+	public NodeImpl[][] getCommunities() {
+		return this.communities;
 	}
 
 	public Graph generate() {
 		Timer timer = new Timer();
 		NodeImpl[] nodes = NodeImpl.init(this.nodes());
 		Random rand = new Random(System.currentTimeMillis());
-		NodeImpl[][] communities = new NodeImpl[this.sizes.length][];
-		// fill communities
-		if (ALTERNATE_NODES) {
-			int[] indices = new int[communities.length];
-			for (int i = 0; i < communities.length; i++) {
-				communities[i] = new NodeImpl[this.sizes[i]];
+		// init communities
+		this.communities = new NodeImpl[this.sizes.length][];
+		for (int i = 0; i < this.communities.length; i++) {
+			this.communities[i] = new NodeImpl[this.sizes[i]];
+		}
+		// fill communities depending on order
+		if (this.order == SORTED_ORDER) {
+			int index = 0;
+			for (int i = 0; i < this.communities.length; i++) {
+				for (int j = 0; j < this.communities[i].length; j++) {
+					this.communities[i][j] = nodes[index++];
+				}
 			}
+		} else if (this.order == ALTERNATING_ORDER) {
+			int[] indices = new int[this.communities.length];
 			for (int i = 0; i < nodes.length;) {
-				for (int j = 0; j < communities.length; j++) {
-					if (indices[j] < communities[j].length) {
-						communities[j][indices[j]++] = nodes[i++];
+				for (int j = 0; j < this.communities.length; j++) {
+					if (indices[j] < this.communities[j].length) {
+						this.communities[j][indices[j]++] = nodes[i++];
 					}
 					if (i >= nodes.length) {
 						break;
 					}
 				}
 			}
-		} else {
-			int index = 0;
-			for (int i = 0; i < communities.length; i++) {
-				communities[i] = new NodeImpl[this.sizes[i]];
-				for (int j = 0; j < communities[i].length; j++) {
-					communities[i][j] = nodes[index++];
+		} else if (this.order == RANDOM_ORDER) {
+			ArrayList<NodeImpl> list = new ArrayList<NodeImpl>(nodes.length);
+			for (int i = 0; i < nodes.length; i++) {
+				list.add(nodes[i]);
+			}
+			for (int i = 0; i < this.communities.length; i++) {
+				for (int j = 0; j < this.communities[i].length; j++) {
+					int index = rand.nextInt(list.size());
+					this.communities[i][j] = list.get(index);
+					list.remove(index);
 				}
 			}
 		}
-		for (int i = 0; i < communities.length; i++) {
-			for (int j = 0; j < communities[i].length; j++) {
-				System.out.println(i + " => " + communities[i][j].index()
-						+ " @ " + communities[i].length);
+		for (int i = 0; i < this.communities.length; i++) {
+			for (int j = 0; j < this.communities[i].length; j++) {
+				System.out.println(i + " => " + this.communities[i][j].index()
+						+ " @ " + this.communities[i].length);
 			}
 		}
 		// create inter-community links
@@ -115,8 +132,8 @@ public class Communities3 extends NetworkImpl implements Network {
 				if (i == j) {
 					continue;
 				}
-				NodeImpl[] from = communities[i];
-				NodeImpl[] to = communities[j];
+				NodeImpl[] from = this.communities[i];
+				NodeImpl[] to = this.communities[j];
 				this
 						.addInterCommunityEdges(from, to, this.p[i][j], rand,
 								edges);
@@ -124,15 +141,15 @@ public class Communities3 extends NetworkImpl implements Network {
 		}
 		edges.fill();
 		// create internal links
-		for (int i = 0; i < communities.length; i++) {
-			for (int j = 0; j < communities[i].length; j++) {
-				NodeImpl n = communities[i][j];
+		for (int i = 0; i < this.communities.length; i++) {
+			for (int j = 0; j < this.communities[i].length; j++) {
+				NodeImpl n = this.communities[i][j];
 				NodeImpl[] inOld = n.in();
 				NodeImpl[] outOld = n.out();
 				NodeImpl[] inNew = new NodeImpl[inOld.length
-						+ communities[i].length - 1];
+						+ this.communities[i].length - 1];
 				NodeImpl[] outNew = new NodeImpl[outOld.length
-						+ communities[i].length - 1];
+						+ this.communities[i].length - 1];
 				int inIndex = 0;
 				int outIndex = 0;
 				for (int k = 0; k < inOld.length; k++) {
@@ -141,10 +158,10 @@ public class Communities3 extends NetworkImpl implements Network {
 				for (int k = 0; k < outOld.length; k++) {
 					outNew[outIndex++] = outOld[k];
 				}
-				for (int k = 0; k < communities[i].length; k++) {
+				for (int k = 0; k < this.communities[i].length; k++) {
 					if (j != k) {
-						inNew[inIndex++] = communities[i][k];
-						outNew[outIndex++] = communities[i][k];
+						inNew[inIndex++] = this.communities[i][k];
+						outNew[outIndex++] = this.communities[i][k];
 					}
 				}
 				n.init(inNew, outNew);
