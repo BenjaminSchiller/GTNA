ec61d3934a48d1b8ef588affc545d9956dd7da14
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2011-05-17 16:16:51 +0200

    adds a GPL licence header to all java source files

--- src/gtna/graph/Edge.java
+++ src/gtna/graph/Edge.java
@@ -1,3 +1,38 @@
+/*
+ * ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ * 
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ * 
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * ---------------------------------------
+ * Edge.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ * 
+ * Original Author: Benjamin Schiller;
+ * Contributors:    -;
+ * 
+ * Changes since 2011-05-17
+ * ---------------------------------------
+*/
 package gtna.graph;
 
 public class Edge {
