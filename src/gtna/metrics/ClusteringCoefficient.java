7a17e8c6aa6f528d7017194e12cb30a438229f2a
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2011-06-01 09:08:00 +0200

    bugfix & performance enhancement for the ClusteringCoefficient metric - excluding v from v's neighborhood - switching from array- to set-based computations, should be faster especially on low-degree graphs - all config parameters starting with CC

--- src/gtna/metrics/ClusteringCoefficient.java
+++ src/gtna/metrics/ClusteringCoefficient.java
@@ -32,12 +32,17 @@
  * 
  * Changes since 2011-05-17
  * ---------------------------------------
-*/
+ * 2011-05-24 : Bugfix - excluding v from v's neighborhood (BS);
+ * 2011-05-24 : Performance - switching from array- to set-based 
+ *              computations, should be faster especially on 
+ *              low-degree graphs (BS);
+ * 2011-05-24 : all config parameters starting with CC (BS);
+ */
 package gtna.metrics;
 
 import gtna.data.Value;
-import gtna.graph.Edge;
 import gtna.graph.Graph;
+import gtna.graph.GraphProperties;
 import gtna.graph.Node;
 import gtna.graph.NodeImpl;
 import gtna.io.DataWriter;
@@ -46,10 +51,11 @@ import gtna.util.Config;
 import gtna.util.Timer;
 import gtna.util.Util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
 
 public class ClusteringCoefficient extends MetricImpl implements Metric {
 	private double[] lcc;
@@ -68,43 +74,39 @@ public class ClusteringCoefficient extends MetricImpl implements Metric {
 
 	public void computeData(Graph g, Network n, Hashtable<String, Metric> m) {
 		this.timer = new Timer();
-		double[] lcc = this.computeLCC(g.nodes, g.map());
-		Arrays.sort(lcc);
-		this.lcc = lcc;
-		this.lccShort = Util.avgArray(lcc, Config
-				.getInt("LCC_SHORT_MAX_VALUES"));
+		boolean onlyOut = GraphProperties.bidirectional(g)
+				|| Config
+						.getBoolean("CC_NEIGHBORHOOD_DEFINED_BY_OUTGOING_EDGES");
+		this.lcc = this.computeLCC(g.nodes, onlyOut);
+		Arrays.sort(this.lcc);
+		this.lccShort = this.computeLCCShort(this.lcc);
 		this.cc = this.computeCC(this.lcc);
 		this.ccw = this.computeCCW(this.lcc, g.nodes);
 		this.timer.end();
 	}
 
-	private double[] computeLCC(NodeImpl[] nodes, HashMap<String, Edge> map) {
+	private double[] computeLCC(NodeImpl[] nodes, boolean onlyOut) {
 		double[] lcc = new double[nodes.length];
 		for (int i = 0; i < nodes.length; i++) {
-			Node[] neighborhood = this.neighborhood(nodes[i]);
-			if (neighborhood.length == 1) {
+			Set<NodeImpl> n = this.neighborhood(nodes[i], onlyOut);
+			if (n.size() <= 1) {
 				lcc[i] = 0;
 			} else {
-				int edges = this.edgesInNeighborhood_NEW(neighborhood,
-						nodes[i], nodes.length, map);
-				lcc[i] = edges
-						/ (double) (neighborhood.length * (neighborhood.length - 1));
+				int edges = this.edgesInNeighborhood(n);
+				lcc[i] = edges / (double) (n.size() * (n.size() - 1));
 			}
 		}
 		return lcc;
 	}
 
-	private int edgesInNeighborhood_NEW(Node[] nh, Node center, int nodes,
-			HashMap<String, Edge> map) {
-		boolean[] in = new boolean[nodes];
-		for (int i = 0; i < nh.length; i++) {
-			in[nh[i].index()] = true;
-		}
+	private int edgesInNeighborhood(Set<NodeImpl> n) {
 		int edges = 0;
-		for (int i = 0; i < nh.length; i++) {
-			Node[] out = nh[i].out();
-			for (int j = 0; j < out.length; j++) {
-				if (in[out[j].index()]) {
+		Iterator<NodeImpl> iter = n.iterator();
+		while (iter.hasNext()) {
+			NodeImpl current = iter.next();
+			NodeImpl[] OUT = current.out();
+			for (NodeImpl out : OUT) {
+				if (n.contains(out)) {
 					edges++;
 				}
 			}
@@ -112,21 +114,23 @@ public class ClusteringCoefficient extends MetricImpl implements Metric {
 		return edges;
 	}
 
-	private Node[] neighborhood(NodeImpl node) {
-		ArrayList<NodeImpl> neighborhood = new ArrayList<NodeImpl>(
-				node.out().length + node.in().length);
-		neighborhood.add(node);
-		NodeImpl[] out = node.out();
-		for (int i = 0; i < out.length; i++) {
-			neighborhood.add(out[i]);
+	private Set<NodeImpl> neighborhood(NodeImpl node, boolean onlyOut) {
+		Set<NodeImpl> n = new HashSet<NodeImpl>();
+		NodeImpl[] OUT = node.out();
+		for (NodeImpl out : OUT) {
+			n.add(out);
 		}
-		NodeImpl[] in = node.in();
-		for (int i = 0; i < in.length; i++) {
-			if (!neighborhood.contains(in[i])) {
-				neighborhood.add(in[i]);
+		if (!onlyOut) {
+			NodeImpl[] IN = node.in();
+			for (NodeImpl in : IN) {
+				n.add(in);
 			}
 		}
-		return Util.toNodeImplArray(neighborhood);
+		return n;
+	}
+
+	private double[] computeLCCShort(double[] lcc) {
+		return Util.avgArray(lcc, Config.getInt("CC_LCC_SHORT_MAX_VALUES"));
 	}
 
 	private double computeCC(double[] lcc) {
@@ -151,16 +155,15 @@ public class ClusteringCoefficient extends MetricImpl implements Metric {
 	}
 
 	public Value[] getValues(Value[] values) {
-		Value CC = new Value("CC", this.cc);
-		Value CCW = new Value("CCW", this.ccw);
-		Value CC_RT = new Value("CC_RT", this.timer.rt());
-		return new Value[] { CC, CCW, CC_RT };
+		Value CC = new Value("CC_CC", this.cc);
+		Value CCW = new Value("CC_CCW", this.ccw);
+		Value RT = new Value("CC_RT", this.timer.rt());
+		return new Value[] { CC, CCW, RT };
 	}
 
 	public boolean writeData(String folder) {
-		DataWriter.writeWithIndex(this.lcc, "LCC", folder);
-		DataWriter.writeWithIndex(this.lccShort, "LCC_SHORT", folder);
+		DataWriter.writeWithIndex(this.lcc, "CC_LCC", folder);
+		DataWriter.writeWithIndex(this.lccShort, "CC_LCC_SHORT", folder);
 		return true;
 	}
-
 }
