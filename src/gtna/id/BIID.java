c7463f6e63fd5fd516168b18ae7ab068c342b4a1
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2011-08-11 02:32:40 +0200

    adds BIID (+Space, +Partition) for ID spaces using BigInteger instead of double

---
+++ src/gtna/id/BIID.java
@@ -0,0 +1,60 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * Identifier.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: benni;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.id;
+
+import java.math.BigInteger;
+
+/**
+ * @author benni
+ * 
+ */
+public interface BIID {
+	/**
+	 * 
+	 * @param id
+	 *            ID to compute the distance to
+	 * @return distance to the given ID
+	 */
+	public BigInteger distance(BIID id);
+
+	/**
+	 * 
+	 * @param id
+	 *            ID to compare to
+	 * @return true if the IDs are equal; false otherwise
+	 */
+	public boolean equals(BIID id);
+}
