facc2b2ea5aef864cab81973ae04fb107737f367
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2011-11-04 12:51:52 +0100

    adds setters for PlaneIdentifier

--- src/gtna/id/plane/PlaneIdentifier.java
+++ src/gtna/id/plane/PlaneIdentifier.java
@@ -114,6 +114,20 @@ public class PlaneIdentifier implements DIdentifier, Comparable<PlaneIdentifier>
 	}
 
 	/**
+	 * @param x the x to set
+	 */
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	/**
 	 * @return the idSpace
 	 */
 	public PlaneIdentifierSpaceSimple getIdSpace() {
