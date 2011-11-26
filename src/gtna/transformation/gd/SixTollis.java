80c266c7106f6322f476539cf7b72a516a2dacd3
Nico Haase <nico@nicohaase.de>
2011-11-26 13:38:53 +0100

    Some more work onto Six/Tollis

--- src/gtna/transformation/gd/SixTollis.java
+++ src/gtna/transformation/gd/SixTollis.java
@@ -39,94 +39,156 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.TreeSet;
+
 import gtna.graph.Edge;
 import gtna.graph.Graph;
 import gtna.graph.Node;
 import gtna.plot.GraphPlotter;
-import gtna.transformation.Transformation;
 
 /**
  * @author Nico
- *
+ * 
  */
 public class SixTollis extends CircularAbstract {
+	private TreeSet<Node> waveCenterNodes, waveFrontNodes, removedNodes;
+	private List<Node> nodeList;
+
 	public SixTollis(int realities, double modulus, boolean wrapAround, GraphPlotter plotter) {
-		super("GDA_SIX_TOLLIS", new String[] {"REALITIES", "MODULUS", "WRAPAROUND"}, new String[] {"" + realities, "" + modulus, "" + wrapAround});
+		super("GDA_SIX_TOLLIS", new String[] { "REALITIES", "MODULUS", "WRAPAROUND" }, new String[] { "" + realities,
+				"" + modulus, "" + wrapAround });
 		this.realities = realities;
 		this.modulus = modulus;
 		this.wrapAround = wrapAround;
 		this.graphPlotter = plotter;
-	}	
+	}
 
 	@Override
 	public Graph transform(Graph g) {
 		System.err.println("This is not working completely yet, so don't expect good results!");
-		
+
 		initIDSpace(g);
-		
-			/*
-			 * Phase 1
-			 */
+
+		/*
+		 * Phase 1
+		 */
+		Node tempNode = null;
 		Node currentNode = null;
-		List<Node> nodeList = Arrays.asList(g.getNodes());
+		Node lastNode = null;
 		List<Edge> removalList = new ArrayList<Edge>();
+		removedNodes = new TreeSet<Node>();
+		waveCenterNodes = new TreeSet<Node>();
+		waveFrontNodes = new TreeSet<Node>();
+
+		nodeList = Arrays.asList(g.getNodes());
 		Collections.sort(nodeList, new NodeDegreeComparator());
-		for ( int counter = 1; counter < ( nodeList.size() - 3 ); counter++ ) {
-			Boolean selectedANodeForThisIteration = false;
-			if ( currentNode != null ) {
-				// try to get a wave front / wave center node
+		for (int counter = 1; counter < (nodeList.size() - 3); counter++) {
+			currentNode = getNode(lastNode);
+			HashSet<Edge> establishedEdges = getPairEdges(g, currentNode);
+
+			/*
+			 * Keep track of wave front and wave center nodes!
+			 */
+
+			waveFrontNodes = new TreeSet<Node>();
+			for (Edge i : currentNode.generateAllEdges()) {
+				int otherEnd;
+				if (i.getDst() == currentNode.getIndex()) {
+					otherEnd = i.getSrc();
+				} else {
+					otherEnd = i.getDst();
+				}
+				tempNode = g.getNode(otherEnd);
+				if (removedNodes.contains(tempNode)) {
+					continue;
+				}
+				waveFrontNodes.add(tempNode);
+				waveCenterNodes.add(tempNode);
 			}
-			if ( !selectedANodeForThisIteration ) {
-				// We got no node yet - choose one with lowest degree
-				currentNode = nodeList.get(0);
-			}
-			List<Edge> establishedEdges = getPairEdges ( currentNode );
+			lastNode = currentNode;
 		}
-		
-		for ( Node n: nodeList ) System.out.println(n + " has degree " + n.getDegree());
-		
-//		countAllCrossings(g);
+
+		for (Node n : nodeList)
+			System.out.println(n + " has degree " + n.getDegree());
+
+		// countAllCrossings(g);
 		writeIDSpace(g);
 		return g;
 	}
-	
-	private List<Edge> getPairEdges ( Node n ) {
-		List<Edge> result = new ArrayList<Edge>();
-		int[] incomingEdges = n.getIncomingEdges();
-		for ( int eDst: incomingEdges ) {
-			if ( isPairEdge( n.getIndex(), eDst ) ) result.add( new Edge(n.getIndex(), eDst));
+
+	/**
+	 * @return
+	 */
+	private Node getNode(Node lastNode) {
+		/*
+		 * Retrieve any wave front node...
+		 */
+		if (waveFrontNodes != null) {
+			for (Node tempNode : waveFrontNodes) {
+				if (!removedNodes.contains(tempNode)) {
+					return tempNode;
+				}
+			}
 		}
-		return result;
+		/*
+		 * ...or a wave center node...
+		 */
+		if (waveCenterNodes != null) {
+			for (Node tempNode : waveCenterNodes) {
+				if (!removedNodes.contains(tempNode)) {
+					return tempNode;
+				}
+			}
+		}
+		/*
+		 * ...or any lowest degree node
+		 */
+		for (Node tempNode : nodeList) {
+			if (!removedNodes.contains(tempNode)) {
+				return tempNode;
+			}
+		}
+		return null;
 	}
-	
-	private Boolean isPairEdge ( int src, int dst ) {
-		// First test: Node src's endPosition is Node dst's start
-		if ( partitions[src].getEnd().getPosition() == partitions[dst].getStart().getPosition() ) {
-			return true;
+
+	private HashSet<Edge> getPairEdges(Graph g, Node n) {
+		HashSet<Edge> result = new HashSet<Edge>();
+		Node tempNode;
+		int otherEnd;
+		
+		throw new RuntimeException("Computing pair edges is not implemented yet");
+		Edge[] allEdges = n.generateAllEdges();
+		for ( Edge tempEdge: allEdges ) {
+			if (tempEdge.getDst() == n.getIndex()) {
+				otherEnd = tempEdge.getSrc();
+			} else {
+				otherEnd = tempEdge.getDst();
+			}
 		}
-		// Second test: Node dst's endPosition is Node src's start		
-		if ( partitions[dst].getEnd().getPosition() == partitions[src].getStart().getPosition() ) {
-			return true;
-		}
-		return false;
+		
+		return result;
 	}
 
 	private class NodeDegreeComparator implements Comparator<Node> {
-		  @Override
-		  public int compare(Node n1, Node n2) {
-			  if ( n1.getDegree() == n2.getDegree() ) return 0;
-			  else if ( n1.getDegree() > n2.getDegree() ) return 1;
-			  else return -1;
-		  }
+		@Override
+		public int compare(Node n1, Node n2) {
+			if (n1.getDegree() == n2.getDegree())
+				return 0;
+			else if (n1.getDegree() > n2.getDegree())
+				return 1;
+			else
+				return -1;
+		}
 	}
-	
+
 	private class NodeRingIDComparator implements Comparator<Node> {
-		  @Override
-		  public int compare(Node n1, Node n2) {
-			  double n1ID = partitions[n1.getIndex()].getStart().getPosition();
-			  double n2ID = partitions[n2.getIndex()].getStart().getPosition();
-			  return Double.compare(n1ID, n2ID);
-		  }
+		@Override
+		public int compare(Node n1, Node n2) {
+			double n1ID = partitions[n1.getIndex()].getStart().getPosition();
+			double n2ID = partitions[n2.getIndex()].getStart().getPosition();
+			return Double.compare(n1ID, n2ID);
+		}
 	}
 }
