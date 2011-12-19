ae747490a77bd348c52bbe3c1f409cfbc0468305
Nico Haase <nico@nicohaase.de>
2011-12-20 00:22:04 +0100

    Revert last commit

--- src/gtna/graph/Graph.java
+++ src/gtna/graph/Graph.java
@@ -182,14 +182,6 @@ public class Graph {
 		return this.nodes;
 	}
 	
-	public int getMaxNodeIndex() {
-		int result = -1;
-		for ( Node singleNode: nodes ) {
-			result = Math.max(result, singleNode.getIndex());
-		}
-		return result;
-	}
-
 	/**
 	 * sets the nodes and ends the timer starte during graph initialization
 	 * 
