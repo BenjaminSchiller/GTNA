a0dc7074765ff7f4af4239b2f66b492621e99d32
Stefanie Roos <stefanieroos@Asmodeus.(none)>
2011-12-23 22:41:59 +0100

    Greedy Variations added

---
+++ src/gtna/routing/greedyVariations/GravityPressureRouting.java
@@ -0,0 +1,196 @@
+package gtna.routing.greddyStef;
+
+import gtna.graph.Graph;
+import gtna.graph.GraphProperty;
+import gtna.graph.Node;
+import gtna.id.BIIdentifier;
+import gtna.id.BIIdentifierSpace;
+import gtna.id.BIPartition;
+import gtna.id.DIdentifier;
+import gtna.id.DIdentifierSpace;
+import gtna.id.DPartition;
+import gtna.routing.Route;
+import gtna.routing.RouteImpl;
+import gtna.routing.RoutingAlgorithmImpl;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GravityPressureRouting extends RoutingAlgorithmImpl{
+	int ttl;
+	 DIdentifierSpace idSpaceD;
+     DPartition[] pD;
+     BIIdentifierSpace idSpaceBI;
+     BIPartition[] pBI;
+     boolean mode; 
+     int[] counts;
+	
+	
+	public GravityPressureRouting() {
+        this(Integer.MAX_VALUE);
+	}
+
+	/**
+	 * @param ttl
+	 */
+	public GravityPressureRouting(int ttl) {
+		super("GRAVITY_PRESSURE", new String[] {"TTL"}, new String[] {""+ttl});
+		
+	}
+
+	
+	@Override
+	public Route routeToRandomTarget(Graph graph, int start, Random rand) {
+		this.setSets(graph.getNodes().length);
+		if (this.idSpaceBI != null) {
+			return this.routeToRandomTargetBI(graph, start, rand);
+		} else if (this.idSpaceD != null) {
+			return this.routeToRandomTargetD(graph, start, rand);
+		} else {
+			return null;
+		}
+	}
+
+	private Route routeToRandomTargetBI(Graph graph, int start, Random rand) {
+		BIIdentifier target = (BIIdentifier) this.idSpaceBI.randomID(rand);
+		while (this.pBI[start].contains(target)) {
+			target = (BIIdentifier) this.idSpaceBI.randomID(rand);
+		}
+		this.mode = true;
+		return this.routeBI(new ArrayList<Integer>(), start, target, rand,
+				graph.getNodes(), this.idSpaceBI.getMaxDistance());
+	}
+
+	private Route routeBI(ArrayList<Integer> route, int current,
+			BIIdentifier target, Random rand, Node[] nodes, BigInteger minDist) {
+		route.add(current);
+		if (this.idSpaceBI.getPartitions()[current].contains(target)) {
+			return new RouteImpl(route, true);
+		}
+		if (route.size() > this.ttl) {
+			return new RouteImpl(route, false);
+		}
+		BigInteger[] next = this.getNextBI(current, target, rand, nodes, minDist);
+		int minNode = next[0].intValue();
+		BigInteger dist = next[1];
+		if (minNode == -1) {
+			return new RouteImpl(route, false);
+		}
+		return this.routeBI(route, minNode, target, rand, nodes, dist);
+	}
+
+	private Route routeToRandomTargetD(Graph graph, int start, Random rand) {
+		DIdentifier target = (DIdentifier) this.idSpaceD.randomID(rand);
+		while (this.pD[start].contains(target)) {
+			target = (DIdentifier) this.idSpaceD.randomID(rand);
+		}
+		this.mode = true;
+		return this.routeD(new ArrayList<Integer>(), start, target, rand,
+				graph.getNodes(), this.idSpaceD.getMaxDistance());
+	}
+
+	private Route routeD(ArrayList<Integer> route, int current,
+			DIdentifier target, Random rand, Node[] nodes, double minDist) {
+		route.add(current);
+		if (this.idSpaceD.getPartitions()[current].contains(target)) {
+			return new RouteImpl(route, true);
+		}
+		if (route.size() > this.ttl) {
+			return new RouteImpl(route, false);
+		}
+		double[] next = this.getNextD(current, target, rand, nodes, minDist);
+		int minNode = (int)next[0];
+		double dist = next[1]; 
+		if (minNode == -1) {
+			return new RouteImpl(route, false);
+		}
+		return this.routeD(route, minNode, target, rand, nodes,minDist);
+	}
+
+	@Override
+	public boolean applicable(Graph graph) {
+		return graph.hasProperty("ID_SPACE_0")
+				&& (graph.getProperty("ID_SPACE_0") instanceof DIdentifierSpace || graph
+						.getProperty("ID_SPACE_0") instanceof BIIdentifierSpace);
+	}
+
+	@Override
+	public void preprocess(Graph graph) {
+		GraphProperty p = graph.getProperty("ID_SPACE_0");
+		if (p instanceof DIdentifierSpace) {
+			this.idSpaceD = (DIdentifierSpace) p;
+			this.pD = (DPartition[]) this.idSpaceD.getPartitions();
+			this.idSpaceBI = null;
+			this.pBI = null;
+		} else if (p instanceof BIIdentifierSpace) {
+			this.idSpaceD = null;
+			this.pD = null;
+			this.idSpaceBI = (BIIdentifierSpace) p;
+			this.pBI = (BIPartition[]) this.idSpaceBI.getPartitions();
+		} else {
+			this.idSpaceD = null;
+			this.pD = null;
+			this.idSpaceBI = null;
+			this.pBI = null;
+		}
+	}
+	
+	public double[] getNextD(int current,
+			DIdentifier target, Random rand, Node[] nodes, double minDist){
+		double currentDist = this.idSpaceD.getPartitions()[current]
+				.distance(target);
+		if (this.mode == false){
+			if (currentDist < minDist){
+				this.mode = true;
+			} else {
+				int minVisits = Integer.MAX_VALUE;
+				double min = Double.MAX_VALUE;
+				int minNode = -1;
+				for (int out : nodes[current].getOutgoingEdges()) {
+					double dist = this.pD[out].distance(target);
+					if (this.counts[out] < minVisits){
+						minNode = out;
+						min = dist;
+						minVisits = this.counts[out];
+					} else {
+						if (this.counts[out] == minVisits && dist < min){
+							minNode = out;
+							min = dist;
+						}
+					}
+				}
+				this.counts[current]++;
+				return new double[]{(double)minNode,minDist};
+			}
+		}
+		if (this.mode){
+			double min = Double.MAX_VALUE;
+			int minNode = -1;
+			for (int out : nodes[current].getOutgoingEdges()) {
+				double dist = this.pD[out].distance(target);
+				if (dist < min){
+					minNode = out;
+					min = dist;
+				} 
+			}
+			if (min < minDist){
+				return new double[]{(double)minNode,min};
+			} else {
+				this.mode = true;
+				return this.getNextD(current, target, rand, nodes, minDist);
+			}
+		}
+		return null;
+	}
+	
+	public BigInteger[] getNextBI(int current,
+			BIIdentifier target, Random rand, Node[] nodes, BigInteger minDist){
+		return null;
+	}
+	
+	public void setSets(int nr){
+		counts = new int[nr];
+	}
+
+}
