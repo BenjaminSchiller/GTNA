b39ae92e1581d1f2864c53c54b6adbc069608c60
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-02-06 12:30:25 +0100

    adds simply example file

---
+++ src/gtna/GTNA.java
@@ -0,0 +1,171 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * GTNA.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: benni;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna;
+
+import gtna.data.Series;
+import gtna.graph.Graph;
+import gtna.io.GraphWriter;
+import gtna.networks.Network;
+import gtna.networks.model.ErdosRenyi;
+import gtna.plot.Plot;
+import gtna.routing.RoutingAlgorithm;
+import gtna.routing.greedy.Greedy;
+import gtna.transformation.Transformation;
+import gtna.transformation.edges.Bidirectional;
+import gtna.transformation.id.RandomRingIDSpace;
+import gtna.util.Config;
+import gtna.util.Util;
+
+/**
+ * @author benni
+ * 
+ */
+public class GTNA {
+	public static void main(String[] args) {
+		Config.overwrite("SKIP_EXISTING_DATA_FOLDER", "false");
+//		GTNA.example1();
+//		 GTNA.example2();
+//		 GTNA.example3();
+//		 GTNA.example4();
+//		 GTNA.example5();
+//		 GTNA.example6();
+		 GTNA.example7();
+	}
+
+	private static void example1() {
+		Network nw1 = new ErdosRenyi(100, 5, false, null, null);
+		Graph g = nw1.generate();
+		GraphWriter.write(g, "./data/firstExample-graph.txt");
+	}
+
+	private static void example2() {
+		Config.overwrite("MAIN_DATA_FOLDER", "./data/example2/");
+		Config.overwrite("MAIN_PLOT_FOLDER", "./plots/example2/");
+		Config.overwrite("METRICS", "DEGREE_DISTRIBUTION");
+
+		Network nw1 = new ErdosRenyi(100, 5, false, null, null);
+		Series s1 = Series.generate(nw1, 5);
+		Plot.multiAvg(s1, "er-unidirectional/");
+
+		Network nw2 = new ErdosRenyi(100, 5, true, null, null);
+		Series s2 = Series.generate(nw2, 10);
+		Plot.multiAvg(s2, "er-bidirectional/");
+
+		Series[] s = new Series[] { s1, s2 };
+		Plot.multiAvg(s, "er-both/");
+	}
+
+	private static void example3() {
+		Config.overwrite("MAIN_DATA_FOLDER", "./data/example2/");
+		Config.overwrite("MAIN_PLOT_FOLDER", "./plots/example3/");
+		Config.overwrite("METRICS", "DEGREE_DISTRIBUTION");
+
+		Network nw1 = new ErdosRenyi(100, 5, false, null, null);
+		Network nw2 = new ErdosRenyi(100, 5, true, null, null);
+		Network[] nw = new Network[] { nw1, nw2 };
+		Series[] s = Series.get(nw);
+		Plot.multiConf(s, "er-get/");
+	}
+
+	private static void example4() {
+		Config.overwrite("MAIN_DATA_FOLDER", "./data/example4/");
+		Config.overwrite("MAIN_PLOT_FOLDER", "./plots/example4/");
+		Config.overwrite("METRICS", "DEGREE_DISTRIBUTION");
+
+		Transformation t1 = new Bidirectional();
+		Transformation[] t = new Transformation[] { t1 };
+		Network nw1 = new ErdosRenyi(100, 5, false, null, null);
+		Network nw2 = new ErdosRenyi(100, 5, false, null, t);
+		Network[] nw = new Network[] { nw1, nw2 };
+		Series[] s = Series.generate(nw, 10);
+		Plot.multiAvg(s, "er-transformed/");
+	}
+
+	private static void example5() {
+		Config.overwrite("MAIN_DATA_FOLDER", "./data/example5/");
+		Config.overwrite("MAIN_PLOT_FOLDER", "./plots/example5/");
+		Config.overwrite("METRICS", "DEGREE_DISTRIBUTION");
+
+		Network[] nw = ErdosRenyi.get(100, new double[] { 5, 6, 7, 8, 9, 10 },
+				false, null, null);
+		Series[] s = Series.generate(nw, 7);
+		Plot.singlesAvg(s, "er-singles/");
+	}
+
+	private static void example6() {
+		Config.overwrite("MAIN_DATA_FOLDER", "./data/example6/");
+		Config.overwrite("MAIN_PLOT_FOLDER", "./plots/example6/");
+		Config.overwrite("METRICS", "CLUSTERING_COEFFICIENT, "
+				+ "DEGREE_DISTRIBUTION, " + "RICH_CLUB_CONNECTIVITY, "
+				+ "ROUTING, " + "SHORTEST_PATHS, " + "STRONG_CONNECTIVITY, "
+				+ "WEAK_CONNECTIVITY");
+
+		Transformation t1 = new RandomRingIDSpace();
+		Transformation t2 = new Bidirectional();
+		Transformation[] t = new Transformation[] { t1, t2 };
+
+		RoutingAlgorithm ra = new Greedy();
+
+		Network[] nw1 = ErdosRenyi.get(100, new double[] { 5, 6, 7, 8, 9, 10 },
+				false, ra, t);
+		Network[] nw2 = ErdosRenyi.get(200, new double[] { 5, 6, 7, 8, 9, 10 },
+				false, ra, t);
+		Network[] nw3 = ErdosRenyi.get(300, new double[] { 5, 6, 7, 8, 9, 10 },
+				false, ra, t);
+		Network[][] nw = new Network[][] { nw1, nw2, nw3 };
+		Series[][] s = Series.generate(nw, 3);
+		Series[] s1 = Series.get(nw1);
+		Series[] s2 = Series.get(nw2);
+		Series[] s3 = Series.get(nw3);
+		Plot.singlesConf(s, "er-more-singles/");
+		Plot.multiConf(s1, "er-more-multi-1/");
+		Plot.multiConf(s2, "er-more-multi-2/");
+		Plot.multiConf(s3, "er-more-multi-3/");
+		Plot.multiConf(Util.combine(s), "er-more-multi/");
+	}
+
+	private static void example7() {
+		Config.overwrite("MAIN_DATA_FOLDER", "./data/example7/");
+		Config.overwrite("MAIN_PLOT_FOLDER", "./plots/example7/");
+		Config.overwrite("METRICS", "SHORTEST_PATHS");
+
+		Config.overwrite("SKIP_EXISTING_DATA_FOLDERS", "true");
+
+		Network nw1 = new ErdosRenyi(100, 5, false, null, null);
+		Series s1 = Series.generate(nw1, 5);
+		Series s2 = Series.generate(nw1, 10);
+	}
+}
