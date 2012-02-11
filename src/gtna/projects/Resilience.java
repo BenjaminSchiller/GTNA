92bb9ba82f268f26e3a3094f845e26a6ffcac14d
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-02-11 16:47:54 +0100

    adds main class for Resilience measurements

---
+++ src/gtna/projects/Resilience.java
@@ -0,0 +1,80 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * Resilience.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: benni;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.projects;
+
+
+/**
+ * @author benni
+ * 
+ */
+public class Resilience {
+
+	// public static void main(String[] args) {
+	// String filename = "./temp/resilience-graph-txt";
+	// // Network nw = new ErdosRenyi(9, 3, false, null, null);
+	// // Graph g = nw.generate();
+	// // GraphWriter.write(g, filename);
+	// Graph g = GraphReader.read(filename);
+	// Resilience.print(g.getNodes());
+	// System.out.println("");
+	//
+	// Random rand = new Random();
+	// NodeSorter sorter = new InDegreeNodeSorter(NodeSorterMode.DESC);
+	// System.out.println(sorter.getKey() + ":");
+	// Resilience.print(sorter.sort(g, rand));
+	// System.out.println();
+	// for (int i = 0; i < 30; i++) {
+	// System.out.println(Resilience.toString(sorter.sort(g, rand)));
+	// // Resilience.print(sorter.sort(g, rand));
+	// // System.out.println();
+	// }
+	// }
+	//
+	// private static void print(Node[] nodes) {
+	// for (int i = 0; i < nodes.length; i++) {
+	// System.out.println("  " + nodes[i]);
+	// }
+	// }
+	//
+	// private static String toString(Node[] nodes) {
+	// StringBuffer buff = new StringBuffer();
+	// for (Node n : nodes) {
+	// buff.append(n.getIndex() + " ");
+	// }
+	// return buff.toString();
+	// }
+
+}
