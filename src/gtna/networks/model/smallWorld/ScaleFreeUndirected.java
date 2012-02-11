ab81a788e85319e3b77af60e5382c58ad82afd21
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-02-11 16:46:38 +0100

    adds ScaleFreeUndirected[] getters for network configurations

--- src/gtna/networks/model/smallWorld/ScaleFreeUndirected.java
+++ src/gtna/networks/model/smallWorld/ScaleFreeUndirected.java
@@ -49,109 +49,154 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Vector;
 
-/** 
- * creating a graph with IDs on a ring and
- *   - a scale-free degree distribution
- *   - undirected
- *   - local links to neighbors within distance C for some constant C
- *   - long-distance links with P(l = d) ~ 1/d, where l is the distance of connected nodes 
+/**
+ * creating a graph with IDs on a ring and - a scale-free degree distribution -
+ * undirected - local links to neighbors within distance C for some constant C -
+ * long-distance links with P(l = d) ~ 1/d, where l is the distance of connected
+ * nodes
+ * 
  * @author stefanie
- *
+ * 
  */
 public class ScaleFreeUndirected extends NetworkImpl {
 	private double interval;
 	private int C;
 	private double alpha;
 	private int cutoff;
-	
-	public ScaleFreeUndirected(int nodes, double alpha, int C, int cutoff, RoutingAlgorithm ra,
-			 Transformation[] t){
-		super("SCALE_FREE_UNDIRECTED", nodes, new String[]{"SIZE", "C", "ALPHA", "CUTOFF"}, 
-				new String[]{""+nodes, ""+C,""+alpha, ""+cutoff}, ra, t); 
-		this.interval = (double)1/nodes;
+
+	public ScaleFreeUndirected(int nodes, double alpha, int C, int cutoff,
+			RoutingAlgorithm ra, Transformation[] t) {
+		super("SCALE_FREE_UNDIRECTED", nodes, new String[] { "C", "ALPHA",
+				"CUTOFF" }, new String[] { "" + C, "" + alpha, "" + cutoff },
+				ra, t);
+		this.interval = (double) 1 / nodes;
 		this.C = C;
 		this.alpha = alpha;
 		this.cutoff = cutoff;
 	}
 
-	/* (non-Javadoc)
+	public static ScaleFreeUndirected[] get(int[] nodes, double alpha, int C,
+			int cutoff, RoutingAlgorithm ra, Transformation[] t) {
+		ScaleFreeUndirected[] nw = new ScaleFreeUndirected[nodes.length];
+		for (int i = 0; i < nodes.length; i++) {
+			nw[i] = new ScaleFreeUndirected(nodes[i], alpha, C, cutoff, ra, t);
+		}
+		return nw;
+	}
+
+	public static ScaleFreeUndirected[] get(int nodes, double[] alpha, int C,
+			int cutoff, RoutingAlgorithm ra, Transformation[] t) {
+		ScaleFreeUndirected[] nw = new ScaleFreeUndirected[alpha.length];
+		for (int i = 0; i < alpha.length; i++) {
+			nw[i] = new ScaleFreeUndirected(nodes, alpha[i], C, cutoff, ra, t);
+		}
+		return nw;
+	}
+
+	public static ScaleFreeUndirected[] get(int nodes, double alpha, int[] C,
+			int cutoff, RoutingAlgorithm ra, Transformation[] t) {
+		ScaleFreeUndirected[] nw = new ScaleFreeUndirected[C.length];
+		for (int i = 0; i < C.length; i++) {
+			nw[i] = new ScaleFreeUndirected(nodes, alpha, C[i], cutoff, ra, t);
+		}
+		return nw;
+	}
+
+	public static ScaleFreeUndirected[] get(int nodes, double alpha, int C,
+			int[] cutoff, RoutingAlgorithm ra, Transformation[] t) {
+		ScaleFreeUndirected[] nw = new ScaleFreeUndirected[cutoff.length];
+		for (int i = 0; i < cutoff.length; i++) {
+			nw[i] = new ScaleFreeUndirected(nodes, alpha, C, cutoff[i], ra, t);
+		}
+		return nw;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see gtna.networks.Network#generate()
 	 */
 	@Override
 	public Graph generate() {
 		Node[] nodes = new Node[this.nodes()];
 		Graph g = new Graph(this.description());
-		for (int i = 0; i < nodes.length; i++){
-			nodes[i] = new Node(i,g);
+		for (int i = 0; i < nodes.length; i++) {
+			nodes[i] = new Node(i, g);
 		}
 		RingIdentifier[] ids = new RingIdentifier[nodes.length];
 		RingPartitionSimple[] parts = new RingPartitionSimple[nodes.length];
-		RingIdentifierSpaceSimple idSpace = new RingIdentifierSpaceSimple(null,1,true);
-		for (int i = 0; i < nodes.length; i++){
-			ids[i] = new RingIdentifier(i*this.interval,idSpace);
+		RingIdentifierSpaceSimple idSpace = new RingIdentifierSpaceSimple(null,
+				1, true);
+		for (int i = 0; i < nodes.length; i++) {
+			ids[i] = new RingIdentifier(i * this.interval, idSpace);
 			parts[i] = new RingPartitionSimple(ids[i]);
 		}
 		idSpace.setPartitions(parts);
-		
-		//label distribution: each node is given a label that determines its potential degree distribution
+
+		// label distribution: each node is given a label that determines its
+		// potential degree distribution
 		double sum = 0;
-		for (int k = 1; k <= this.cutoff; k++){
-			sum = sum + Math.pow((double)k,-this.alpha);
+		for (int k = 1; k <= this.cutoff; k++) {
+			sum = sum + Math.pow((double) k, -this.alpha);
 		}
 		int[] labels = new int[nodes.length];
 		double[] randNum = new double[nodes.length];
 		Vector<Integer> vec = new Vector<Integer>(nodes.length);
 		Random rand = new Random();
-		for (int i = 0; i < nodes.length; i++){
+		for (int i = 0; i < nodes.length; i++) {
 			vec.add(i);
 			randNum[i] = rand.nextDouble();
 		}
 		Arrays.sort(randNum);
 		int count = 0;
 		double s = 0;
-		for (int k = 1; k < this.cutoff; k++){
-			s = s + Math.pow((double)k,-this.alpha);
-			while (randNum.length > count && vec.size() > 0 && randNum[count]*sum < s){
+		for (int k = 1; k < this.cutoff; k++) {
+			s = s + Math.pow((double) k, -this.alpha);
+			while (randNum.length > count && vec.size() > 0
+					&& randNum[count] * sum < s) {
 				count++;
 				labels[vec.remove(rand.nextInt(vec.size()))] = k;
 			}
 		}
-		for (int k = 0; k < vec.size(); k++){
+		for (int k = 0; k < vec.size(); k++) {
 			labels[vec.get(k)] = this.cutoff;
 		}
-		
-		Edges edges = new Edges(nodes,(int)Math.round(sum*this.nodes()));
-		
-		//create local edges: randomly choose node within distance C
-		for (int i = 0; i < nodes.length; i++){
-			int n1 = (i + rand.nextInt(this.C)+1) % nodes.length;
+
+		Edges edges = new Edges(nodes, (int) Math.round(sum * this.nodes()));
+
+		// create local edges: randomly choose node within distance C
+		for (int i = 0; i < nodes.length; i++) {
+			int n1 = (i + rand.nextInt(this.C) + 1) % nodes.length;
 			edges.add(i, n1);
-			edges.add(n1,i);
-			int n2 = (i - rand.nextInt(this.C)-1 + nodes.length) % nodes.length;
+			edges.add(n1, i);
+			int n2 = (i - rand.nextInt(this.C) - 1 + nodes.length)
+					% nodes.length;
 			edges.add(i, n2);
-			edges.add(n2,i);
+			edges.add(n2, i);
 		}
-		//create long-range edges: use labels and distance for each pair of nodes
+		// create long-range edges: use labels and distance for each pair of
+		// nodes
 		double norm = 0;
-		for (int i = 1; i <= nodes.length/2; i++){
-			norm = norm + 2/(i*this.interval);
+		for (int i = 1; i <= nodes.length / 2; i++) {
+			norm = norm + 2 / (i * this.interval);
 		}
-		for (int i = 0; i < nodes.length; i++){
-			for (int j = i+1; j < nodes.length; j++){
-				
-					double p = 1 - Math.exp(- (labels[i]*labels[j])/(norm*ids[i].distance(ids[j])));
-					if (rand.nextDouble() < p){
-						edges.add(i,j);
-						edges.add(j, i);
-					}
-				
+		for (int i = 0; i < nodes.length; i++) {
+			for (int j = i + 1; j < nodes.length; j++) {
+
+				double p = 1 - Math.exp(-(labels[i] * labels[j])
+						/ (norm * ids[i].distance(ids[j])));
+				if (rand.nextDouble() < p) {
+					edges.add(i, j);
+					edges.add(j, i);
+				}
+
 			}
 		}
-		
+
 		g.setNodes(nodes);
 		edges.fill();
 		g.addProperty(g.getNextKey("ID_SPACE"), idSpace);
-		
+
 		return g;
 	}
 
