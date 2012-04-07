2df646db9127263bffac60b07298279c7b5fa9d8
Nam Truong Le <lenamtruong@gmail.com>
2012-04-07 23:01:03 +0200

    erstelle ClosenessCentralityNodeSorter

---
+++ src/gtna/graph/sorting/ClosenessCentralityNodeSorter.java
@@ -0,0 +1,88 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * ClosenessCentralityNodeSorter.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: truong;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.graph.sorting;
+
+import gtna.graph.Graph;
+import gtna.graph.Node;
+
+import java.util.Random;
+
+/**
+ * @author truong
+ * 
+ */
+public class ClosenessCentralityNodeSorter extends NodeSorter {
+
+	public ClosenessCentralityNodeSorter(NodeSorterMode mode) {
+		super("CLOSENESS", mode);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gtna.graph.sorting.NodeSorter#sort(gtna.graph.Graph,
+	 * java.util.Random)
+	 */
+	@Override
+	public Node[] sort(Graph g, Random rand) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gtna.graph.sorting.NodeSorter#applicable(gtna.graph.Graph)
+	 */
+	@Override
+	public boolean applicable(Graph g) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see gtna.graph.sorting.NodeSorter#isPropertyEqual(gtna.graph.Node,
+	 * gtna.graph.Node)
+	 */
+	@Override
+	protected boolean isPropertyEqual(Node n1, Node n2) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
