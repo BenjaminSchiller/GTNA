061b83b1a81bb87d3660dbe10e15c98d91dc2cdb
Stefanie Roos <stef-roos@web.de>
2012-04-13 14:09:08 +0200

    absolute value in distance check

--- src/gtna/transformation/attackableEmbedding/IQD/IdentifierNode.java
+++ src/gtna/transformation/attackableEmbedding/IQD/IdentifierNode.java
@@ -88,7 +88,7 @@ public abstract class IdentifierNode extends IQDNode {
 			if (this.embedding.isCheckold()){
 				boolean found = false;
 				for (int i = 0; i < knownIDs.length; i++){
-					if (this.embedding.computeDistance(knownIDs[i], this.id) < this.embedding.getEpsilon()){
+					if (Math.abs(this.embedding.computeDistance(knownIDs[i], this.id)) < this.embedding.getEpsilon()){
 						found = true;
 						break;
 					}
@@ -111,7 +111,7 @@ public abstract class IdentifierNode extends IQDNode {
 			if (this.embedding.isCheckold()){
 				boolean found = false;
 				for (int i = 0; i < knownIDs.length; i++){
-					if (this.embedding.computeDistance((sort[i]+sort[(i+1)%sort.length])/(double)2, this.id) < this.embedding.getEpsilon()){
+					if (Math.abs(this.embedding.computeDistance((sort[i]+sort[(i+1)%sort.length])/(double)2, this.id)) < this.embedding.getEpsilon()){
 						found = true;
 						break;
 					}
@@ -128,7 +128,7 @@ public abstract class IdentifierNode extends IQDNode {
 			if (this.embedding.isCheckold()){
 				found = false;
 				for (int i = 0; i < knownIDs.length; i++){
-					if (this.embedding.computeDistance(knownIDs[i], this.id) < this.embedding.getEpsilon()){
+					if (Math.abs(this.embedding.computeDistance(knownIDs[i], this.id)) < this.embedding.getEpsilon()){
 						found = true;
 						break;
 					}
@@ -158,7 +158,7 @@ public abstract class IdentifierNode extends IQDNode {
 			if (this.embedding.isCheckold()){
 				found = false;
 				for (int i = 0; i < knownIDs.length; i++){
-					if (this.embedding.computeDistance((sort[i]+sort[(i+1)%sort.length])/(double)2, this.id) < this.embedding.getEpsilon()){
+					if (Math.abs(this.embedding.computeDistance((sort[i]+sort[(i+1)%sort.length])/(double)2, this.id)) < this.embedding.getEpsilon()){
 						found = true;
 						break;
 					}
@@ -174,7 +174,7 @@ public abstract class IdentifierNode extends IQDNode {
 			for (int i = 1; i < this.knownIDs.length; i++){
 				res[i] = (sort[i]+sort[i-1])/(double)2 - this.embedding.getEpsilon()*rand.nextDouble();
 			}
-			res[this.knownIDs.length-1] = (1+sort[0]+sort[sort.length-1])/2 + this.embedding.getEpsilon()*rand.nextDouble();
+			res[this.knownIDs.length-1] = (1+sort[0]+sort[sort.length-1])/2 - this.embedding.getEpsilon()*rand.nextDouble();
 			while (res[res.length-1] > 1){
 				res[res.length-1]--;
 			}
