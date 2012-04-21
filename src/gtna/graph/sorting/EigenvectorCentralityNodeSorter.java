6dc559dd50b544b5289f0e862466c68b754a4012
Nam Truong Le <lenamtruong@gmail.com>
2012-04-22 01:23:18 +0200

    implement: eigenvector centrality (iteration approx. ), but was not tested

--- src/gtna/graph/sorting/EigenvectorCentralityNodeSorter.java
+++ src/gtna/graph/sorting/EigenvectorCentralityNodeSorter.java
@@ -37,7 +37,9 @@ package gtna.graph.sorting;
 
 import gtna.graph.Graph;
 import gtna.graph.Node;
-
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -45,6 +47,9 @@ import java.util.Random;
  * 
  */
 public class EigenvectorCentralityNodeSorter extends NodeSorter {
+
+	private HashMap<Node, Double> map = new HashMap<Node, Double>();
+	private int numRuns = 100;
 
 	/**
 	 * @param key
@@ -63,8 +68,74 @@ public class EigenvectorCentralityNodeSorter extends NodeSorter {
 	 */
 	@Override
 	public Node[] sort(Graph g, Random rand) {
-		// TODO Auto-generated method stub
-		return null;
+		this.calculate(g);
+		Node[] sorted = this.clone(g.getNodes());
+		Arrays.sort(sorted, new DegreeAsc());
+		this.randomize(sorted, rand);
+		if (this.mode == NodeSorterMode.DESC) {
+			sorted = this.reverse(sorted);
+		}
+		return sorted;
+	}
+
+	private class DegreeAsc implements Comparator<Node> {
+		public int compare(Node n1, Node n2) {
+			double point1 = EigenvectorCentralityNodeSorter.this.map.get(n1)
+					.doubleValue();
+			double point2 = EigenvectorCentralityNodeSorter.this.map.get(n2)
+					.doubleValue();
+			int result;
+			if (point1 == point2) {
+				result = 0;
+			} else if (point1 > point2) {
+				result = 1;
+			} else {
+				result = -1;
+			}
+			return result;
+		}
+	}
+
+	/**
+	 * @param g2
+	 */
+	private void calculate(Graph g) {
+		int N = g.getNodes().length;
+		double[] tmp = new double[N];
+		double[] centralities = new double[N];
+
+		int count = 0;
+
+		HashMap<Integer, Node> indicies = new HashMap<Integer, Node>();
+		HashMap<Node, Integer> invIndices = new HashMap<Node, Integer>();
+		for (Node u : g.getNodes()) {
+			indicies.put(count, u);
+			invIndices.put(u, count);
+			centralities[count] = 1;
+			count++;
+		}
+
+		for (int s = 0; s < numRuns; s++) {
+			double max = 0;
+			for (int i = 0; i < N; i++) {
+				Node u = indicies.get(i);
+				for (int id : u.getOutgoingEdges()) {
+					tmp[i] += centralities[id];
+				}
+				max = Math.max(max, tmp[i]);
+			}
+			// sumChange = 0;
+			for (int k = 0; k < N; k++) {
+				if (max != 0) {
+					// sumChange += Math.abs(centralities[k] - (tmp[k] / max));
+					centralities[k] = tmp[k] / max;
+				}
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			map.put(g.getNode(i), centralities[i]);
+		}
 	}
 
 	/*
@@ -74,8 +145,7 @@ public class EigenvectorCentralityNodeSorter extends NodeSorter {
 	 */
 	@Override
 	public boolean applicable(Graph g) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	/*
@@ -86,8 +156,11 @@ public class EigenvectorCentralityNodeSorter extends NodeSorter {
 	 */
 	@Override
 	protected boolean isPropertyEqual(Node n1, Node n2) {
-		// TODO Auto-generated method stub
-		return false;
+		return map.get(n1).doubleValue() == map.get(n2).doubleValue();
+	}
+
+	public double getCentrality(Node n) {
+		return map.get(n);
 	}
 
 }
