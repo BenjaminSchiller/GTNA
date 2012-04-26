46f8a220e3c3d3852cd7cb52b65d35b354af42bf
Philipp Neubrand <Flipp9@freenet.de>
2012-04-27 00:52:56 +0200

    adds GirvanAndNewman Wrapper to CondonAndKarp network

--- src/gtna/networks/model/GirvanAndNewman.java
+++ src/gtna/networks/model/GirvanAndNewman.java
@@ -21,7 +21,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * ---------------------------------------
- * CondonAndKarp.java
+ * GirvanAndNewman.java
  * ---------------------------------------
  * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
  * and Contributors 
@@ -35,76 +35,29 @@
  */
 package gtna.networks.model;
 
-import gtna.graph.Edges;
 import gtna.graph.Graph;
-import gtna.graph.Node;
 import gtna.networks.Network;
 import gtna.transformation.Transformation;
 import gtna.util.parameter.DoubleParameter;
-import gtna.util.parameter.IntParameter;
 import gtna.util.parameter.Parameter;
+import gtna.util.parameter.ParameterList;
 
 /**
  * @author Philipp Neubrand
- * 
- *         Brandes, U., M. Gaertler, and D. Wagner, 2003, in Proceed- ings of
- *         ESA (Springer-Verlag, Berlin, Germany), pp. 568{ 579.
- * 
+ *
  */
-public class CondonAndKarp extends Network {
+public class GirvanAndNewman extends Network {
+	
+	private CondonAndKarp nw;
 
-	private int groups;
-	private double pin;
-	private double pout;
-
-	/**
-	 * @param key
-	 * @param nodes
-	 * @param parameters
-	 * @param transformations
-	 */
-	public CondonAndKarp(int nodes, int groups, double pin, double pout,
-			Transformation[] transformations) {
-		super("CONDON_KARP", nodes, new Parameter[] {
-				new IntParameter("GROUPS", groups),
-				new DoubleParameter("PIN", pin),
-				new DoubleParameter("POUT", pout) }, transformations);
-
-		this.groups = groups;
-		this.pin = pin;
-		this.pout = pout;
+	public GirvanAndNewman(double pin, Transformation[] t){
+		super("GIRVAN_NEWMAN", 128, new Parameter[]{new DoubleParameter("Pin", pin)}, t);
+		nw = new CondonAndKarp(128, 4, pin, (0.5 - pin) / 3.0, t);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see gtna.networks.Network#generate()
-	 */
 	@Override
 	public Graph generate() {
-		Graph graph = new Graph(this.getDescription());
-
-		Node[] nodes = Node.init(this.getNodes(), graph);
-		int nodesPerCom = getNodes() / groups;
-		Edges edges = new Edges(nodes, 1);
-
-		for (int i = 0; i < getNodes(); i++) {
-			for (int j = 0; j < getNodes(); j++) {
-				if (i == j)
-					continue;
-
-				if (i / nodesPerCom == j / nodesPerCom) {
-					if (Math.random() < pin)
-						edges.add(i, j);
-				} else if (Math.random() < pout)
-					edges.add(i, j);
-			}
-
-		}
-
-		edges.fill();
-		graph.setNodes(nodes);
-		return graph;
+		return nw.generate();
 	}
 
 }
