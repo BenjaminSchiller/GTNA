f63745c7456f944b440343ba811adde3a3002a62
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-04-28 18:16:33 +0200

    changes diff param selection to prefer parameter != nodes

--- src/gtna/util/parameter/ParameterList.java
+++ src/gtna/util/parameter/ParameterList.java
@@ -81,7 +81,7 @@ public class ParameterList {
 			buff.append("-" + p.getValue());
 		}
 		String folderName = buff.toString();
-		if(folderName.length() > 255){
+		if (folderName.length() > 255) {
 			return Integer.toString(folderName.hashCode());
 		}
 		return folderName;
@@ -219,18 +219,23 @@ public class ParameterList {
 		if (pl2.getParameters().length != this.parameters.length) {
 			return null;
 		}
+		Parameter backup = null;
 		for (int i = 0; i < this.parameters.length; i++) {
 			Parameter p1 = this.parameters[i];
 			Parameter p2 = pl2.getParameters()[i];
 			if (!p1.getKey().equals(p2.getKey())) {
 				return null;
 			}
+			if (p1.getKey().equals("NODES")) {
+				backup = p1;
+				continue;
+			}
 			if (!p1.getValue().equals(p2.getValue())
 					&& (p1 instanceof DoubleParameter || p1 instanceof IntParameter)) {
 				return p1;
 			}
 		}
-		return null;
+		return backup;
 	}
 
 	public String getDiffParameterNameXY(ParameterList pl2, String xy) {
@@ -297,6 +302,15 @@ public class ParameterList {
 				return "T";
 			} else {
 				return "Times";
+			}
+		}
+		if (p.getKey().equals("INDEX")) {
+			if (xy.endsWith("_LONG")) {
+				return "Index";
+			} else if (xy.endsWith("_SHORT")) {
+				return "I";
+			} else {
+				return "Index";
 			}
 		}
 		return Config.get(this.key + "_" + p.getKey() + xy);
