d2df41d764cc3f4d9d038e4ab5e8f01df2712ffb
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-04-30 10:40:04 +0200

    updates ResourcesGeneration

--- src/gtna/projects/resources/ResourcesGeneration.java
+++ src/gtna/projects/resources/ResourcesGeneration.java
@@ -40,6 +40,7 @@ import gtna.graph.Graph;
 import gtna.io.graphReader.CaidaGraphReader;
 import gtna.io.graphReader.CoauthorGraphReader;
 import gtna.io.graphReader.GraphReader;
+import gtna.io.graphReader.GtnaGraphReader;
 import gtna.io.graphReader.SpiGraphReader;
 import gtna.io.graphReader.WotGraphReader;
 import gtna.io.graphWriter.GtnaGraphWriter;
@@ -48,6 +49,11 @@ import gtna.metrics.basic.DegreeDistribution;
 import gtna.networks.Network;
 import gtna.networks.util.ReadableFile;
 import gtna.plot.Plotting;
+import gtna.transformation.Transformation;
+import gtna.transformation.edges.Bidirectional;
+import gtna.transformation.partition.LargestStronglyConnectedComponent;
+import gtna.transformation.partition.LargestWeaklyConnectedComponent;
+import gtna.transformation.remove.RemoveLargestNode;
 import gtna.util.Config;
 import gtna.util.filenameFilter.ExcludeHiddenFilenameFilter;
 import gtna.util.filenameFilter.PostfixFilenameFilter;
@@ -63,65 +69,149 @@ import java.io.File;
 public class ResourcesGeneration {
 
 	public static void main(String[] args) throws Exception {
-		ResourcesGeneration.read(new CaidaGraphReader(), "caida");
-		// ResourcesGeneration.read(new WotGraphReader(), "wot");
-		// ResourcesGeneration.read(new SpiGraphReader(), "spi");
-		// ResourcesGeneration.read(new CoauthorGraphReader(), "coauthor");
+		Metric dd = new DegreeDistribution();
+		Metric[] m = new Metric[] { dd };
+
+		Transformation[][] t_caida = new Transformation[][] {
+				new Transformation[] { new LargestStronglyConnectedComponent() },
+				new Transformation[] { new LargestWeaklyConnectedComponent() },
+				new Transformation[] { new LargestWeaklyConnectedComponent(),
+						new Bidirectional() } };
+
+		Transformation[][] t_coauthor = new Transformation[][] {
+				new Transformation[] { new LargestStronglyConnectedComponent() },
+				new Transformation[] { new LargestWeaklyConnectedComponent() },
+				new Transformation[] { new LargestWeaklyConnectedComponent(),
+						new Bidirectional() } };
+
+		Transformation[][] t_spi = new Transformation[][] {
+				new Transformation[] { new RemoveLargestNode() },
+				new Transformation[] { new RemoveLargestNode(),
+						new LargestStronglyConnectedComponent() },
+				new Transformation[] { new RemoveLargestNode(),
+						new LargestWeaklyConnectedComponent() },
+				new Transformation[] { new RemoveLargestNode(),
+						new LargestWeaklyConnectedComponent(),
+						new Bidirectional() } };
+
+		Transformation[][] t_wot = new Transformation[][] {
+				new Transformation[] { new LargestStronglyConnectedComponent() },
+				new Transformation[] { new LargestWeaklyConnectedComponent() },
+				new Transformation[] { new LargestWeaklyConnectedComponent(),
+						new Bidirectional() } };
+
+		ResourcesGeneration.generate(new CaidaGraphReader(), "caida", t_caida,
+				m);
+		ResourcesGeneration.generate(new CoauthorGraphReader(), "coauthor",
+				t_coauthor, m);
+		ResourcesGeneration.generate(new SpiGraphReader(), "spi", t_spi, m);
+		ResourcesGeneration.generate(new WotGraphReader(), "wot", t_wot, m);
 	}
 
-	public static void read(GraphReader graphReader, String type) {
-		String srcFolder = "./resources/" + type + "/source/";
-		String dstFolder = "./resources/" + type + "/original/";
-		String evalFolder = "./resources/" + type + "/original-eval/";
+	public static void generate(GraphReader graphReader, String type,
+			Transformation[][] T, Metric[] metrics) {
+		String folder = "resources/" + type + "/";
 
+		String srcFolder = folder + "source/";
 		if (!(new File(srcFolder)).exists()) {
 			return;
 		}
 
-		if (!(new File(dstFolder)).exists()) {
-			(new File(dstFolder)).mkdirs();
+		String originalFolder = folder + "original/";
+		if (!(new File(originalFolder)).exists()) {
+			(new File(originalFolder)).mkdirs();
 		}
 
-		File[] files = new File(srcFolder)
-				.listFiles(new ExcludeHiddenFilenameFilter());
-		for (File f : files) {
-			String dest = dstFolder + f.getName() + ".gtna";
-			if ((new File(dest)).exists()) {
-				continue;
-			}
-			System.out.println(graphReader.getKey() + ": " + f.getName());
-			Graph g = graphReader.read(f.getAbsolutePath());
-			new GtnaGraphWriter().write(g, dest);
-		}
-
-		ResourcesGeneration.evaluate(type, dstFolder, evalFolder);
+		generateOriginal(graphReader, type, srcFolder, originalFolder);
+		generateTransformed(type, folder, originalFolder, T);
+		evaluation(type, folder, originalFolder, T, metrics);
 	}
 
-	public static void evaluate(String type, String graphFolder,
-			String evalFolder) {
-		Config.overwrite("MAIN_DATA_FOLDER", evalFolder + "data/");
-		Config.overwrite("MAIN_PLOT_FOLDER", evalFolder + "plots-");
-		Config.overwrite("SKIP_EXISTING_DATA_FOLDERS", "true");
+	public static void evaluation(String type, String folder,
+			String originalFolder, Transformation[][] T, Metric[] metrics) {
+		Config.overwrite("MAIN_DATA_FOLDER", folder + "evaluation-data/");
+		Config.overwrite("MAIN_PLOT_FOLDER", folder + "evaluation-plots/");
 
-		boolean get = false;
-
-		File[] graphs = new File(graphFolder)
+		File[] originalGraphs = new File(originalFolder)
 				.listFiles(new PostfixFilenameFilter(".gtna"));
-		Network[] nw = new Network[graphs.length];
-		for (int i = 0; i < graphs.length; i++) {
-			nw[i] = new ReadableFile(type + " - " + graphs[i].getName(), type,
-					graphs[i].getAbsolutePath(),
-					new Parameter[] { new IntParameter("INDEX", i) }, null);
+		Network[][] nw = new Network[T.length + 1][originalGraphs.length];
+		for (int j = 0; j < originalGraphs.length; j++) {
+			nw[0][j] = new ReadableFile(type, type,
+					originalGraphs[j].getAbsolutePath(),
+					new Parameter[] { new IntParameter("INDEX", j) }, null);
+		}
+		for (int i = 0; i < T.length; i++) {
+			for (int j = 0; j < originalGraphs.length; j++) {
+				nw[i + 1][j] = new ReadableFile(type + toString(T[i]), type
+						+ toString(T[i]), originalGraphs[j].getAbsolutePath(),
+						new Parameter[] { new IntParameter("INDEX", j) }, T[i]);
+			}
 		}
 
-		Metric dd = new DegreeDistribution();
-		Metric[] metrics = new Metric[] { dd };
+		Series[][] s = Series.generate(nw, metrics, 1);
 
-		Series[] s = get ? Series.get(nw, metrics) : Series.generate(nw,
-				metrics, 1);
+		Plotting.single(s, metrics, "all/");
+		Plotting.multi(s[0], metrics, "original/");
+		for (int i = 0; i < T.length; i++) {
+			Plotting.multi(s[i + 1], metrics, toString(T[i]) + "/");
+		}
+	}
 
-		Plotting.multi(s, metrics, "multi-");
-		Plotting.single(s, metrics, "single-");
+	public static void generateOriginal(GraphReader graphReader, String type,
+			String srcFolder, String originalFolder) {
+		File[] srcGraphs = new File(srcFolder)
+				.listFiles(new ExcludeHiddenFilenameFilter());
+		for (File srcGraph : srcGraphs) {
+			String original = originalFolder + srcGraph.getName() + ".gtna";
+			if ((new File(original)).exists()) {
+				continue;
+			}
+			System.out.println(type + ": original " + srcGraph.getName());
+			Graph g = graphReader.read(srcGraph.getAbsolutePath());
+			new GtnaGraphWriter().writeWithProperties(g, original);
+		}
+	}
+
+	public static void generateTransformed(String type, String folder,
+			String originalFolder, Transformation[][] T) {
+		for (Transformation[] t : T) {
+			String transformedFolder = folder + toString(t) + "/";
+			generateTransformed(type, originalFolder, transformedFolder, t);
+		}
+	}
+
+	public static void generateTransformed(String type, String originalFolder,
+			String transformedFolder, Transformation[] t) {
+		File[] originalGraphs = new File(originalFolder)
+				.listFiles(new PostfixFilenameFilter(".gtna"));
+		for (File originalGraph : originalGraphs) {
+			String transformed = transformedFolder + originalGraph.getName();
+			if ((new File(transformed)).exists()) {
+				continue;
+			}
+			System.out.println(type + ": transformation "
+					+ originalGraph.getName() + " @ " + toString(t));
+			Graph g = new GtnaGraphReader().readWithProperties(originalGraph
+					.getAbsolutePath());
+			for (Transformation t1 : t) {
+				if (!t1.applicable(g)) {
+					continue;
+				}
+				g = t1.transform(g);
+			}
+			new GtnaGraphWriter().writeWithProperties(g, transformed);
+		}
+	}
+
+	public static String toString(Transformation[] t) {
+		if (t == null) {
+			return "";
+		}
+		String string = "";
+		for (Transformation t1 : t) {
+			string += "_" + t1.getDescriptionShort();
+		}
+		return string;
 	}
 
 }
