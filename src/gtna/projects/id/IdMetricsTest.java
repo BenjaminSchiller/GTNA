59c2f516ff64de999142b153447817853cc853da
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-05-03 16:30:30 +0200

    updates project classes for testing community-based embedding and id-space metrics

--- src/gtna/projects/id/IdMetricsTest.java
+++ src/gtna/projects/id/IdMetricsTest.java
@@ -50,15 +50,25 @@ import gtna.networks.model.placementmodels.Partitioner;
 import gtna.networks.model.placementmodels.PlacementModel;
 import gtna.networks.model.placementmodels.PlacementModelContainer;
 import gtna.networks.model.placementmodels.connectors.UDGConnector;
+import gtna.networks.model.placementmodels.models.CirclePlacementModel;
 import gtna.networks.model.placementmodels.models.CommunityPlacementModel;
+import gtna.networks.model.placementmodels.models.GridPlacementModel;
+import gtna.networks.model.placementmodels.models.RandomPlacementModel;
+import gtna.networks.model.placementmodels.models.CirclePlacementModel.DistributionType;
 import gtna.networks.model.placementmodels.partitioners.SimplePartitioner;
 import gtna.networks.util.DescriptionWrapper;
 import gtna.plot.Plotting;
 import gtna.transformation.Transformation;
 import gtna.transformation.attackableEmbedding.lmc.LMC;
 import gtna.transformation.communities.CommunityDetectionLPA;
+import gtna.transformation.embedding.communities.CommunityEmbedding;
 import gtna.transformation.embedding.communities.SimpleCommunityEmbedding1;
 import gtna.transformation.embedding.communities.SimpleCommunityEmbedding2;
+import gtna.transformation.embedding.communities.partitioner.community.EqualSizeCommunityPartitioner;
+import gtna.transformation.embedding.communities.partitioner.idSpace.EqualSizeIdSpacePartitioner;
+import gtna.transformation.embedding.communities.partitioner.idSpace.RelativeSizeIdSpacePartitioner;
+import gtna.transformation.embedding.communities.sorter.community.RandomCommunitySorter;
+import gtna.transformation.embedding.communities.sorter.node.RandomNodeSorter;
 import gtna.transformation.id.RandomRingIDSpace;
 import gtna.transformation.id.RandomRingIDSpaceSimple;
 import gtna.transformation.util.Nothing;
@@ -83,6 +93,14 @@ public class IdMetricsTest {
 		Config.overwrite("SKIP_EXISTING_DATA_FOLDERS", "false");
 		Config.overwrite("SERIES_GRAPH_WRITE", "false");
 		Config.overwrite("GNUPLOT_PRINT_ERRORS", "false");
+		Config.overwrite("GNUPLOT_LW", "5");
+
+		IdMetricsTest.testPlane();
+		// IdMetricsTest.testRing();
+
+		if (true) {
+			return;
+		}
 
 		boolean GET = false;
 		int TIMES = 1;
@@ -96,8 +114,8 @@ public class IdMetricsTest {
 		Metric ridsshd = new RingIdentifierSpaceSuccessorHopDistances();
 		Metric sp = new ShortestPaths();
 		Metric didsdp = new DIdentifierSpaceDistanceProducts(1000);
-		Metric[] metrics = new Metric[] { idsd, idsv1, idsv2, idsv3 };
-		metrics = new Metric[] { didsdp };
+		Metric[] metrics = new Metric[] { idsd, idsv1, idsv2, idsv3, pidsv,
+				pidsv };
 
 		Transformation[] rand1 = new Transformation[] {
 				new RemoveGraphProperty(), new RandomRingIDSpaceSimple() };
@@ -152,9 +170,88 @@ public class IdMetricsTest {
 		stats.end();
 	}
 
+	public static void testRing() {
+		Metric idsd = new DIdentifierSpaceDistances(100);
+		Metric idsv1 = new RingIdentifierSpaceVisualzation(100);
+		Metric idsv2 = new RingIdentifierSpaceVisualzation(500);
+		Metric idsv3 = new RingIdentifierSpaceVisualzation();
+		Metric ridsshd = new RingIdentifierSpaceSuccessorHopDistances();
+		Metric didsdp = new DIdentifierSpaceDistanceProducts(1000);
+		Metric[] metrics = new Metric[] { idsd, idsv1, idsv2, idsv3, ridsshd,
+				didsdp };
+
+		Transformation[] t1 = new Transformation[] { new RemoveGraphProperty(),
+				new RandomRingIDSpaceSimple() };
+		Transformation[] t2 = new Transformation[] { new RemoveGraphProperty(),
+				new RandomRingIDSpaceSimple(),
+				new LMC(1000, LMC.MODE_UNRESTRICTED, 0, LMC.DELTA_1_N, 0) };
+		Transformation[] t3 = new Transformation[] {
+				new RemoveGraphProperty(),
+				new CommunityDetectionLPA(50),
+				new CommunityEmbedding(new RandomCommunitySorter(),
+						new EqualSizeIdSpacePartitioner(0.0),
+						new RandomNodeSorter(),
+						new EqualSizeCommunityPartitioner(), 1.0, true) };
+		Transformation[] t4 = new Transformation[] {
+				new RemoveGraphProperty(),
+				new CommunityDetectionLPA(50),
+				new CommunityEmbedding(new RandomCommunitySorter(),
+						new RelativeSizeIdSpacePartitioner(0.0),
+						new RandomNodeSorter(),
+						new EqualSizeCommunityPartitioner(), 1.0, true) };
+
+		Map<Transformation[], String> names = new HashMap<Transformation[], String>();
+		names.put(t1, "random");
+		names.put(t2, "lmc");
+		names.put(t3, "ce equal");
+		names.put(t4, "ce relative");
+
+		int nodes = 1000;
+
+		Network nw1 = IdMetricsTest.nwCC(nodes, t1, names);
+		Network nw2 = IdMetricsTest.nwCC(nodes, t2, names);
+		Network nw3 = IdMetricsTest.nwCC(nodes, t3, names);
+		Network nw4 = IdMetricsTest.nwCC(nodes, t4, names);
+
+		Network[] nw = new Network[] { nw1, nw2, nw3, nw4 };
+
+		Series[] s = Series.generate(nw, metrics, 1);
+
+		Plotting.multi(s, metrics, "ring/");
+	}
+
+	public static void testPlane() {
+		Metric pidsv = new PlaneIdentifierSpaceVisualization();
+		Metric[] metrics = new Metric[] { pidsv };
+
+		Transformation[] t1 = new Transformation[] { new Nothing(
+				new IntParameter("INDEX", 1)) };
+		Transformation[] t2 = new Transformation[] { new Nothing(
+				new IntParameter("INDEX", 2)) };
+		Transformation[] t3 = new Transformation[] { new Nothing(
+				new IntParameter("INDEX", 3)) };
+
+		Map<Transformation[], String> names = new HashMap<Transformation[], String>();
+		names.put(t1, "nw1");
+		names.put(t2, "nw2");
+		names.put(t3, "nw3");
+
+		int nodes = 2000;
+
+		Network nw1 = IdMetricsTest.nwCC(nodes, t1, names);
+		Network nw2 = IdMetricsTest.nwR(nodes, t2, names);
+		Network nw3 = IdMetricsTest.nwGC(nodes, t3, names);
+
+		Network[] nw = new Network[] { nw1, nw2, nw3 };
+
+		Series[] s = Series.generate(nw, metrics, 1);
+
+		Plotting.multi(s, metrics, "plane/");
+	}
+
 	private static final double xyNodes = 40000;
 	private static final double xyHotspots = 30000;
-	private static final double devHotspots = 5000;
+	private static final double devHotspots = 10000;
 	private static final double devNodes = 2000;
 	private static final double radius = 1983;
 
@@ -162,7 +259,8 @@ public class IdMetricsTest {
 
 	private static final NodeConnector connector = new UDGConnector(radius);
 
-	public static Network nwCC(int nodes, Transformation[] t) {
+	public static Network nwCC(int nodes, Transformation[] t,
+			Map<Transformation[], String> names) {
 		PlacementModel p1 = new CommunityPlacementModel(devHotspots,
 				devHotspots, false);
 		PlacementModel p2 = new CommunityPlacementModel(devNodes, devNodes,
@@ -170,6 +268,26 @@ public class IdMetricsTest {
 		Network nw = new PlacementModelContainer(nodes, nodes / 100, xyNodes,
 				xyNodes, xyHotspots, xyHotspots, p1, p2, partitioner,
 				connector, t);
-		return new DescriptionWrapper(nw, "Communities " + nodes);
+		return new DescriptionWrapper(nw, names.get(t));
+	}
+
+	public static Network nwR(int nodes, Transformation[] t,
+			Map<Transformation[], String> names) {
+		PlacementModel p1 = new RandomPlacementModel(xyNodes, xyNodes, true);
+		PlacementModel p2 = new RandomPlacementModel(xyNodes, xyNodes, false);
+		Network nw = new PlacementModelContainer(nodes, 1, xyNodes, xyNodes,
+				xyNodes, xyNodes, p1, p2, partitioner, connector, t);
+		return new DescriptionWrapper(nw, names.get(t));
+	}
+
+	public static Network nwGC(int nodes, Transformation[] t,
+			Map<Transformation[], String> names) {
+		PlacementModel p1 = new GridPlacementModel(xyNodes / 1.5,
+				xyNodes / 1.5, 5, 5, false);
+		PlacementModel p2 = new CirclePlacementModel(radius * 1.2,
+				DistributionType.FIXED, DistributionType.FIXED, false);
+		Network nw = new PlacementModelContainer(nodes, 23, xyNodes, xyNodes,
+				xyNodes, xyNodes, p1, p2, partitioner, connector, t);
+		return new DescriptionWrapper(nw, names.get(t));
 	}
 }
