5202f9c82f5491a8f80b08a130874d52f9e3c49a
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-07-23 16:07:56 +0200

    adds comments to the Graph class

--- src/gtna/graph/Graph.java
+++ src/gtna/graph/Graph.java
@@ -35,32 +35,134 @@
  */
 package gtna.graph;
 
-import gtna.util.Timer;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Graph {
-	private String name;
-
-	private Node[] nodes;
-
-	private HashMap<String, GraphProperty> properties;
 
 	/**
-	 * initializes a graph with the given name an starts the timer
 	 * 
 	 * @param name
 	 *            name of the graph
 	 */
 	public Graph(String name) {
 		this.name = name;
-		this.properties = new HashMap<String, GraphProperty>();
+	}
+
+	public Graph(String name, int nodes) {
+		this.name = name;
+		this.nodes = Node.init(nodes, this);
 	}
 
 	public String toString() {
 		return this.name + " (" + this.nodes.length + ")";
 	}
+
+	/*
+	 * NAME
+	 */
+
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/*
+	 * NODES
+	 */
+
+	private Node[] nodes = new Node[0];
+
+	/**
+	 * @return the nodes
+	 */
+	public Node[] getNodes() {
+		return this.nodes;
+	}
+
+	/**
+	 * @param nodes
+	 *            the nodes to set
+	 */
+	public void setNodes(Node[] nodes) {
+		this.nodes = nodes;
+	}
+
+	/**
+	 * @param nodeIndex
+	 * @return the node with index nodeIndex
+	 */
+	public Node getNode(int nodeIndex) {
+		return this.nodes[nodeIndex];
+	}
+
+	public int getNodeCount() {
+		return this.nodes.length;
+	}
+
+	/*
+	 * EDGES
+	 */
+
+	private Edges edges = null;
+
+	public int computeNumberOfEdges() {
+		int E = 0;
+		for (Node n : this.nodes) {
+			E += n.getOutDegree();
+		}
+		return E;
+	}
+
+	public Edge[] generateEdges() {
+		int E = 0;
+		for (Node n : this.nodes) {
+			E += n.getOutDegree();
+		}
+		Edge[] edges = new Edge[E];
+		int index = 0;
+		for (Node n : this.nodes) {
+			for (int out : n.getOutgoingEdges()) {
+				edges[index++] = new Edge(n.getIndex(), out);
+			}
+		}
+		return edges;
+	}
+
+	public Edges getEdges() {
+		if (this.edges != null) {
+			return this.edges;
+		}
+		int E = 0;
+		for (Node n : this.nodes) {
+			E += n.getOutDegree();
+		}
+		this.edges = new Edges(this.nodes, E);
+		for (Node n : this.nodes) {
+			for (int out : n.getOutgoingEdges()) {
+				this.edges.add(n.getIndex(), out);
+			}
+		}
+		return this.edges;
+	}
+
+	/*
+	 * PROPERTIES
+	 */
+
+	private HashMap<String, GraphProperty> properties = new HashMap<String, GraphProperty>();
 
 	public void addProperty(String key, GraphProperty property) {
 		this.properties.put(key, property);
@@ -98,87 +200,5 @@ public class Graph {
 
 	public HashMap<String, GraphProperty> getProperties() {
 		return this.properties;
-	}
-
-	public int computeNumberOfEdges() {
-		int E = 0;
-		for (Node n : this.nodes) {
-			E += n.getOutDegree();
-		}
-		return E;
-	}
-
-	public Edge[] generateEdges() {
-		int E = 0;
-		for (Node n : this.nodes) {
-			E += n.getOutDegree();
-		}
-		Edge[] edges = new Edge[E];
-		int index = 0;
-		for (Node n : this.nodes) {
-			for (int out : n.getOutgoingEdges()) {
-				edges[index++] = new Edge(n.getIndex(), out);
-			}
-		}
-		return edges;
-	}
-
-	private Edges edges = null;
-
-	public Edges getEdges() {
-		if (this.edges != null) {
-			return this.edges;
-		}
-		int E = 0;
-		for (Node n : this.nodes) {
-			E += n.getOutDegree();
-		}
-		this.edges = new Edges(this.nodes, E);
-		for (Node n : this.nodes) {
-			for (int out : n.getOutgoingEdges()) {
-				this.edges.add(n.getIndex(), out);
-			}
-		}
-		return this.edges;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return the nodes
-	 */
-	public Node[] getNodes() {
-		return this.nodes;
-	}
-	
-	/**
-	 * sets the nodes and ends the timer starte during graph initialization
-	 * 
-	 * @param nodes
-	 *            the nodes to set
-	 */
-	public void setNodes(Node[] nodes) {
-		this.nodes = nodes;
-	}
-
-	/**
-	 * @param nodeIndex
-	 * @return the node with index nodeIndex
-	 */
-	public Node getNode(int nodeIndex) {
-		return nodes[nodeIndex];
 	}
 }
