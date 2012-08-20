72400ef918b8d5323fd6182ce048d158f66b60b2
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-08-21 00:12:29 +0200

    implements writer and reader for NodeColors

--- src/gtna/drawing/NodeColors.java
+++ src/gtna/drawing/NodeColors.java
@@ -36,6 +36,9 @@
 package gtna.drawing;
 
 import gtna.graph.GraphProperty;
+import gtna.io.Filereader;
+import gtna.io.Filewriter;
+import gtna.util.Config;
 
 import java.awt.Color;
 
@@ -59,16 +62,68 @@ public class NodeColors extends GraphProperty {
 		return this.colors;
 	}
 
+	protected String asString(Color c) {
+		return c.getRed() + "/" + c.getGreen() + "/" + c.getBlue();
+	}
+
+	protected Color fromString(String s) {
+		String[] temp = s.split("/");
+		int r = Integer.parseInt(temp[0]);
+		int g = Integer.parseInt(temp[1]);
+		int b = Integer.parseInt(temp[2]);
+		return new Color(r, g, b);
+	}
+
 	@Override
 	public boolean write(String filename, String key) {
-		// TODO implement GraphProperty.write(...)
-		return false;
+		Filewriter fw = new Filewriter(filename);
+
+		// CLASS
+		fw.writeComment(Config.get("GRAPH_PROPERTY_CLASS"));
+		fw.writeln(this.getClass().getCanonicalName().toString());
+
+		// KEYS
+		fw.writeComment(Config.get("GRAPH_PROPERTY_KEY"));
+		fw.writeln(key);
+
+		// # OF NODES
+		fw.writeComment("# of Nodes");
+		fw.writeln(this.colors.length);
+
+		fw.writeln();
+
+		// LIST OF NODE COLORS
+		for (Color color : this.colors) {
+			fw.writeln(this.asString(color));
+		}
+
+		return fw.close();
 	}
 
 	@Override
 	public String read(String filename) {
-		// TODO implement GraphProperty.read(...)
-		return null;
+		Filereader fr = new Filereader(filename);
+
+		// CLASS
+		fr.readLine();
+
+		// KEYS
+		String key = fr.readLine();
+
+		// # OF NODES
+		int nodes = Integer.parseInt(fr.readLine());
+		this.colors = new Color[nodes];
+
+		// NODE COLORS
+		String line = null;
+		int index = 0;
+		while ((line = fr.readLine()) != null) {
+			this.colors[index++] = this.fromString(line);
+		}
+
+		fr.close();
+
+		return key;
 	}
 
 }
