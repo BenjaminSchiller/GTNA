96d504b11f933b5d268ca6d31549e28f5d04c6e9
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-09-12 07:33:24 +0200

    adds routing algorithm based on RoutingTable GraphProperty

---
+++ src/gtna/routing/routingTable/RoutingTableRouting.java
@@ -0,0 +1,108 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * RoutingTableRouting.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: benni;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.routing.routingTable;
+
+import gtna.graph.Graph;
+import gtna.graph.Node;
+import gtna.id.Identifier;
+import gtna.routing.Route;
+import gtna.routing.RoutingAlgorithm;
+import gtna.routing.table.RoutingTables;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author benni
+ * 
+ */
+@SuppressWarnings("rawtypes")
+public class RoutingTableRouting extends RoutingAlgorithm {
+
+	private static final int ttl = 100;
+
+	protected RoutingTables rt;
+
+	public RoutingTableRouting() {
+		super("ROUTING_TABLE_ROUTING");
+	}
+
+	public void preprocess(Graph graph) {
+		super.preprocess(graph);
+		if (this.hasRoutingTable(graph)) {
+			this.rt = (RoutingTables) graph.getProperty("ROUTING_TABLES_0");
+		}
+	}
+
+	@Override
+	public Route routeToTarget(Graph graph, int start, Identifier target,
+			Random rand) {
+		System.out.print(target + " @ " + start);
+		return this.routeToTarget(new ArrayList<Integer>(), start, target,
+				graph.getNodes());
+	}
+
+	public Route routeToTarget(ArrayList<Integer> route, int current,
+			Identifier target, Node[] nodes) {
+		System.out.print(" => " + current);
+		route.add(current);
+		if (this.isEndPoint(current, target)) {
+			System.out.println("\n" + current + " is endpoint for " + target);
+			return new Route(route, true);
+		}
+		if (route.size() > ttl) {
+			return new Route(route, false);
+		}
+		int nextHop = this.rt.getRoutingTable(current).getNextHop(target);
+		if (nextHop == RoutingTables.noRoute) {
+			return new Route(route, false);
+		}
+		return this.routeToTarget(route, nextHop, target, nodes);
+	}
+
+	protected boolean hasRoutingTable(Graph graph) {
+		return graph.hasProperty("ROUTING_TABLES_0")
+				&& graph.getProperty("ROUTING_TABLES_0") instanceof RoutingTables;
+	}
+
+	@Override
+	public boolean applicable(Graph graph) {
+		return this.hasRoutingTable(graph)
+				&& (this.hasIdentifierSpace(graph) || this
+						.hasDataStorage(graph));
+	}
+
+}
