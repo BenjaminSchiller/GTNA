57dab93cd8d53dbd2491c95a91349035552b5ab6
Benjamin Schiller <schiller@cs.tu-darmstadt.de>
2012-09-12 07:28:49 +0200

    adds Dijkstra Algorithm

---
+++ src/gtna/algorithms/shortestPaths/Dijkstra.java
@@ -0,0 +1,123 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * Dijkstra.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: benni;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.algorithms.shortestPaths;
+
+import gtna.graph.Graph;
+import gtna.routing.table.RoutingTables;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author benni
+ * 
+ */
+public class Dijkstra {
+	/**
+	 * 
+	 * @param graph
+	 * @return {int[][] dist, int[][] previous, int[][] nextHop} for all nodes
+	 *         in $graph
+	 */
+	public static int[][][] getShortestPaths(Graph graph) {
+		int[][][] sp = new int[3][graph.getNodeCount()][];
+		for (int i = 0; i < sp.length; i++) {
+			int[][] temp = Dijkstra.getShortestPaths(graph, i);
+			sp[0][i] = temp[0];
+			sp[1][i] = temp[1];
+			sp[2][i] = temp[2];
+		}
+		return sp;
+	}
+
+	/**
+	 * http://en.wikipedia.org/wiki/Dijkstra's_algorithm#Pseudocode
+	 * 
+	 * @param graph
+	 * @param start
+	 * @return {int[] dist, int[] previous, int[] nextHop} for node $start
+	 */
+	public static int[][] getShortestPaths(Graph graph, int start) {
+		int[] dist = new int[graph.getNodeCount()];
+		int[] previous = new int[graph.getNodeCount()];
+		int[] nextHop = new int[graph.getNodeCount()];
+		Set<Integer> Q = new HashSet<Integer>();
+
+		for (int i = 0; i < graph.getNodeCount(); i++) {
+			dist[i] = Integer.MAX_VALUE;
+			previous[i] = RoutingTables.noRoute;
+			nextHop[i] = RoutingTables.noRoute;
+			Q.add(i);
+		}
+
+		dist[start] = 0;
+		nextHop[start] = start;
+
+		Set<Integer> neighbors = new HashSet<Integer>();
+		for (int out : graph.getNode(start).getOutgoingEdges()) {
+			neighbors.add(out);
+		}
+
+		while (!Q.isEmpty()) {
+			int u = Dijkstra.getMinElement(Q, dist);
+			Q.remove(u);
+
+			if (dist[u] == Integer.MAX_VALUE)
+				break;
+
+			for (int v : graph.getNode(u).getOutgoingEdges()) {
+				int alt = dist[u] + 1;
+				if (alt < dist[v]) {
+					dist[v] = alt;
+					previous[v] = u;
+					nextHop[v] = nextHop[u];
+				}
+			}
+		}
+
+		return new int[][] { dist, previous, nextHop };
+	}
+
+	private static int getMinElement(Set<Integer> Q, int[] dist) {
+		int min = Q.iterator().next();
+		for (int element : Q) {
+			if (dist[element] < dist[min]) {
+				min = element;
+			}
+		}
+		return min;
+	}
+}
