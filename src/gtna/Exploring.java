514e9a1ab878581bb95997249aa0ca0e5cb7e78e
Tim Grube <tim2307@gmail.com>
2013-06-24 16:57:12 +0200

    exploring gtna functionality

--- src/gtna/Exploring.java
+++ src/gtna/Exploring.java
@@ -36,6 +36,7 @@
 package gtna;
 
 import gtna.data.Series;
+<<<<<<< HEAD
 import gtna.drawing.Gephi;
 import gtna.graph.Graph;
 import gtna.id.IdentifierSpace;
@@ -163,6 +164,24 @@ import gtna.util.Stats;
 
 import java.io.File;
 import java.util.Arrays;
+=======
+import gtna.metrics.Metric;
+import gtna.metrics.basic.ClusteringCoefficient;
+import gtna.metrics.basic.DegreeDistribution;
+import gtna.metrics.basic.ShortestPaths;
+import gtna.metrics.connectivity.RichClubConnectivity;
+import gtna.networks.Network;
+import gtna.networks.model.BarabasiAlbert;
+import gtna.networks.model.CondonAndKarp;
+import gtna.networks.model.ErdosRenyi;
+import gtna.networks.model.WattsStrogatz;
+import gtna.plot.Gnuplot.Style;
+import gtna.plot.Plotting;
+import gtna.plot.data.Data.Type;
+import gtna.transformation.Transformation;
+import gtna.transformation.edges.Bidirectional;
+import gtna.util.Config;
+>>>>>>> exploring gtna functionality
 
 /**
  * @author Tim
@@ -173,6 +192,7 @@ public class Exploring {
 	public static void main(String[] args) {
 		Config.overwrite("SKIP_EXISTING_DATA_FOLDERS", "false");
 		// Config.overwrite("GNUPLOT_PRINT_ERRORS", "true");
+<<<<<<< HEAD
 		
 		
 <<<<<<< HEAD
@@ -651,5 +671,43 @@ public class Exploring {
 			
 			System.out.println(filename);
 		}
+=======
+
+		Transformation t = new Bidirectional();
+
+		Network nw1 = new ErdosRenyi(200, 10, false, new Transformation[]{t});
+		Network nw2 = new BarabasiAlbert(200, 10, new Transformation[]{t});
+		Network nw3 = new WattsStrogatz(200, 6, 0.2, new Transformation[]{t});
+		/*
+		 * CC -> 19500msec to 20500msec 
+		 */
+		Network nw4 = new CondonAndKarp(200, 3, 0.4, 0.05, new Transformation[]{t});
+
+		Metric[] metrics = new Metric[] { new DegreeDistribution(),
+				new ShortestPaths(), new ClusteringCoefficient()};
+		// .generate for new networks
+		// .get for loading networks
+//		Series s1 = Series.get(nw1, metrics/*, 5*/);
+//		Series s2 = Series.get(nw2, metrics/*, 5*/);
+//		Series s3 = Series.get(nw3, metrics/*, 5*/);
+//		Series s4 = Series.get(nw4, metrics/*, 5*/);
+
+		Series s1 = Series.generate(nw1, metrics, 5);
+		Series s2 = Series.generate(nw2, metrics, 5);
+		Series s3 = Series.generate(nw3, metrics, 5);
+		Series s4 = Series.generate(nw4, metrics, 5);
+
+		
+		Series[] s = new Series[] { s1, s2, s3, s4 };
+
+		if (!Plotting.single(s, metrics, "example-s/")) {
+//			System.err.println("Failed plotting single values");
+		}
+//		Plotting.multi(s, metrics, "example-m/", Type.confidence1,
+//				Style.candlesticks);
+		 if(Plotting.multi(s, metrics, "example-m/")){
+//		 System.err.println("Failed plotting multi values");
+		 }
+>>>>>>> exploring gtna functionality
 	}
 }
