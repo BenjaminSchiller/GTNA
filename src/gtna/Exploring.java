3bef7e73f5a4fa25590540c217c1be28e0440413
Tim Grube <tim2307@gmail.com>
2013-06-27 09:36:22 +0200

    Plotting of a regular network added.

--- src/gtna/Exploring.java
+++ src/gtna/Exploring.java
@@ -48,11 +48,13 @@ import gtna.networks.Network;
 import gtna.networks.model.BarabasiAlbert;
 import gtna.networks.model.CondonAndKarp;
 import gtna.networks.model.ErdosRenyi;
+import gtna.networks.model.Regular;
 import gtna.networks.model.WattsStrogatz;
 import gtna.plot.Plotting;
 import gtna.transformation.Transformation;
 import gtna.transformation.id.RandomPlaneIDSpaceSimple;
 import gtna.transformation.id.RandomRingIDSpace;
+import gtna.transformation.id.RandomRingIDSpaceSimple;
 import gtna.transformation.id.node.NodeIds;
 import gtna.util.Config;
 
@@ -72,16 +74,18 @@ public class Exploring {
 		
 //		Transformation t = new Bidirectional();
 
+		Network nw0 = new Regular(10, 2, true, false, null);
 		Network nw1 = new ErdosRenyi(10, 3, false, null);
 		Network nw2 = new BarabasiAlbert(10, 2, null);
 		Network nw3 = new WattsStrogatz(10, 2, 0.2, null);
 		Network nw4 = new CondonAndKarp(10, 2, 0.4, 0.05, null);
-		Network nw5 = new ErdosRenyi(100, 10, false, null);
-		Network nw6 = new BarabasiAlbert(100, 10, null);
-		Network nw7 = new WattsStrogatz(100, 6, 0.2, null);
-		Network nw8 = new CondonAndKarp(100, 4, 0.4, 0.05, null);
+		Network nw5 = new Regular(100, 5, true, false, null);
+		Network nw6 = new ErdosRenyi(100, 10, false, null);
+		Network nw7 = new BarabasiAlbert(100, 10, null);
+		Network nw8 = new WattsStrogatz(100, 6, 0.2, null);
+		Network nw9 = new CondonAndKarp(100, 4, 0.4, 0.05, null);
 		
-		Network[] n = new Network[] {nw1, nw2, nw3, nw4, nw5, nw6, nw7, nw8};
+		Network[] n = new Network[] {nw0, nw1, nw2, nw3, nw4, nw5, nw6, nw7, nw8, nw9};
 		
 		Metric[] metrics = new Metric[] { 
 				new DegreeDistribution(),
@@ -110,22 +114,34 @@ public class Exploring {
 	
 	public static void plot(Network nw, String filename, int times) {
 		Transformation t_rpid = new RandomPlaneIDSpaceSimple(1, 100, 100, true);
-		Transformation t_nid = new NodeIds();
+		Transformation t_rrid = new RandomRingIDSpaceSimple(true);
+		
+		
+		Transformation t_nid = t_rpid;
 		
 		
 		for (int i = 0; i < times; i++) {
 			Gephi gephi = new Gephi();
-			Config.overwrite("GEPHI_RING_RADIUS", "1");
-			Config.overwrite("GEPHI_NODE_BORDER_WIDTH", "0.01");
-			Config.overwrite("GEPHI_EDGE_SCALE", "0.001");
-			Config.overwrite("GEPHI_DRAW_CURVED_EDGES", "true");
-			Config.overwrite("GEPHI_NODE_SIZE", "0.1");
+			if(t_nid == t_rpid){
+				Config.overwrite("GEPHI_RING_RADIUS", "1");
+				Config.overwrite("GEPHI_NODE_BORDER_WIDTH", "0.01");
+				Config.overwrite("GEPHI_EDGE_SCALE", "0.001");
+				Config.overwrite("GEPHI_DRAW_CURVED_EDGES", "true");
+				Config.overwrite("GEPHI_NODE_SIZE", "0.1");
+			}else 
+				if(t_nid == t_rrid){
+					Config.overwrite("GEPHI_RING_RADIUS", "50");
+					Config.overwrite("GEPHI_NODE_BORDER_WIDTH", "0.01");
+					Config.overwrite("GEPHI_EDGE_SCALE", "0.001");
+					Config.overwrite("GEPHI_DRAW_CURVED_EDGES", "true");
+					Config.overwrite("GEPHI_NODE_SIZE", "0.001");	
+			}
 
 			String graphFilename = filename;
 			
 			Graph g = nw.generate();
 			
-			g = t_rpid.transform(g);
+			g = t_nid.transform(g);
 	
 			IdentifierSpace ids = (IdentifierSpace) g
 					.getProperty("ID_SPACE_0");
