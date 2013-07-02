a03fa3fd01a3c3c4f83aec297ecbd053faf397c8
Tim Grube <tim2307@googlemail.com>
2013-07-02 22:43:13 +0200

    SamplingController is a Transformation ASampler, AStartNodeSelector, AWalkerController, NetworkSample is a Parameter Further parameters: dimension, revisiting, scaledown

--- src/gtna/transformation/sampling/AStartNodeSelector.java
+++ src/gtna/transformation/sampling/AStartNodeSelector.java
@@ -35,10 +35,12 @@
  */
 package gtna.transformation.sampling;
 
+import gtna.util.parameter.Parameter;
+
 /**
  * @author Tim
  *
  */
-public abstract class AStartNodeSelector {
+public abstract class AStartNodeSelector extends Parameter{
 
 }
