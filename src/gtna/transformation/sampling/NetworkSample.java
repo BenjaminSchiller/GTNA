6260fec1573c751c8f0e368a7bdbc8ec69c1dcf6
Tim Grube <tim2307@googlemail.com>
2013-07-08 00:07:01 +0200

    code style, minor changes in method-/class-design

--- src/gtna/transformation/sampling/NetworkSample.java
+++ src/gtna/transformation/sampling/NetworkSample.java
@@ -38,6 +38,7 @@ package gtna.transformation.sampling;
 import gtna.graph.Node;
 import gtna.util.parameter.Parameter;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -45,7 +46,7 @@ import java.util.Map;
 
 /**
  * @author Tim
- *
+ * 
  */
 public class NetworkSample extends Parameter {
 
@@ -58,126 +59,154 @@ public class NetworkSample extends Parameter {
 	 */
 	public NetworkSample(String key, String value) {
 		super(key, value);
-		
+
 		sampleNodeMapping = new HashMap<Integer, Integer>();
 		revisitFrequency = new HashMap<Integer, List<Integer>>();
 	}
-	
-	
-		
-	
+
 	/**
 	 * Add the given node in the given round to the sample
-	 * @param n			Node, added to the sample
-	 * @param round		Round in which the node is sampled
-	 * @return			true if added
+	 * 
+	 * @param n
+	 *            Nodes, added to the sample
+	 * @param round
+	 *            Round in which the node is sampled
+	 * @return true if added
 	 */
-	public boolean addNodeToSample(Node n, int round){
-		if(!initialized()){throw new IllegalStateException("NetworkSample is not initialized!");}
-		if(!uniqueRound(round)){throw new IllegalArgumentException("round has to be unique but round = "+round+" was already seen");}
-		
-		
-		if(!sampleNodeMapping.containsKey(n.getIndex())){
-			// add node to the sample and initialize the RF for this node
-			int newId = sampleNodeMapping.size();
-			sampleNodeMapping.put(n.getIndex(), newId);
-			
-			List<Integer> rF = new LinkedList<Integer>();
-			rF.add(round);
-			revisitFrequency.put(n.getIndex(), rF);
-		} else {
-			// no need to add the node to the sample again, just add it to the RF
-			List<Integer> rF = revisitFrequency.get(n.getIndex());
-			rF.add(round);
-			revisitFrequency.put(n.getIndex(), rF);
+	public boolean addNodeToSample(Collection<Node> nodes, int round) {
+		if (!initialized()) {
+			throw new IllegalStateException("NetworkSample is not initialized!");
 		}
-		
-		
-		
+		if (!uniqueRound(round)) {
+			throw new IllegalArgumentException(
+					"round has to be unique but round = " + round
+							+ " was already seen");
+		}
+
+		for (Node n : nodes) {
+			if (!sampleNodeMapping.containsKey(n.getIndex())) {
+				// add node to the sample and initialize the RF for this node
+				int newId = sampleNodeMapping.size();
+				sampleNodeMapping.put(n.getIndex(), newId);
+
+				List<Integer> rF = new LinkedList<Integer>();
+				rF.add(round);
+				revisitFrequency.put(n.getIndex(), rF);
+			} else {
+				// no need to add the node to the sample again, just add it to
+				// the RF
+				List<Integer> rF = revisitFrequency.get(n.getIndex());
+				rF.add(round);
+				revisitFrequency.put(n.getIndex(), rF);
+			}
+		}
+
 		return true;
 	}
-	
+
 	/**
 	 * @param n
 	 * @return
 	 */
 	public boolean contains(Node n) {
-		if(sampleNodeMapping.containsKey(n.getIndex())){
+		if (sampleNodeMapping.containsKey(n.getIndex())) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	/**
-	 * Return the RevisitFrequency-Map for the whole sampling
-	 * Key = nodeID (from the node in the original graph)
-	 * Value = List of the rounds in which the node was visited
+	 * Return the RevisitFrequency-Map for the whole sampling Key = nodeID (from
+	 * the node in the original graph) Value = List of the rounds in which the
+	 * node was visited
 	 * 
-	 * (in this default implementation Value.get(1) is the nodeId in the subgraph induced by the sampling)
+	 * 
 	 * @return
 	 */
-	public Map<Integer, List<Integer>> getRevisitFrequency(){
+	public Map<Integer, List<Integer>> getRevisitFrequency() {
 		return revisitFrequency;
 	}
-	
+
 	/**
 	 * Return the revisitFrequency for the given nodeId
+	 * 
 	 * @param nodeId
-	 * @return	List of rounds in which the given node is visited.
+	 * @return List of rounds in which the given node is visited.
 	 */
-	public List<Integer> getRevisitFrequency(int nodeId){
-		if(revisitFrequency.containsKey(nodeId)){
+	public List<Integer> getRevisitFrequency(int nodeId) {
+		if (revisitFrequency.containsKey(nodeId)) {
 			return revisitFrequency.get(nodeId);
-		} 
-		
+		}
+
 		return new LinkedList<Integer>();
 	}
-	
+
 	/**
-	 * Return the sample as Map of:
-	 * 	Key = nodeID in the original graph
-	 *  Value = nodeID in the subgraph induced by the sampling
+	 * Return the sample as Map of: Key = nodeID in the original graph Value =
+	 * nodeID in the subgraph induced by the sampling
+	 * 
 	 * @return
 	 */
-	public Map<Integer, Integer> getSampleNodeMapping(){
+	public Map<Integer, Integer> getSampleNodeMapping() {
 		return sampleNodeMapping;
 	}
-	
+
 	/**
 	 * returns the current size of the sample
+	 * 
 	 * @return
 	 */
-	public int getSampleSize(){
+	public int getSampleSize() {
 		return sampleNodeMapping.size();
 	}
-	
+
 	/**
-	 * Checks if the necessary maps are initialized 
+	 * Checks if the necessary maps are initialized
+	 * 
 	 * @return
 	 */
-	private boolean initialized(){
-		if(sampleNodeMapping != null && revisitFrequency != null){
+	private boolean initialized() {
+		if (sampleNodeMapping != null && revisitFrequency != null) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
-	 * checks if the given round is already known
-	 * known = any node is visited in this round
+	 * checks if the given round is already known known = any node is visited in
+	 * this round
+	 * 
 	 * @param round
 	 * @return
 	 */
-	private boolean uniqueRound(int round){
-		for(List<Integer> l : revisitFrequency.values()){
-			if(l.contains(round)){
+	private boolean uniqueRound(int round) {
+		for (List<Integer> l : revisitFrequency.values()) {
+			if (l.contains(round)) {
 				return false;
 			}
 		}
-		
+
 		return true;
+	}
+	
+	/**
+	 * Returns the new Id of the sampled node 
+	 * @param n	node in the original graph
+	 * @return	nodeId in the sampled graph
+	 */
+	public int getNewIndexOfSampledNode(Node n){
+		return getNewIndexOfSampledNode(n.getIndex());
+	}
+	
+	/**
+	 * Returns the new Id of the sampled node 
+	 * @param n	nodeId in the original graph
+	 * @return	nodeId in the sampled graph
+	 */
+	public int getNewIndexOfSampledNode(int n){
+		return sampleNodeMapping.get(n);
 	}
 
 }
