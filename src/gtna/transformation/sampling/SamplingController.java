9309319e346a4970c3574d827b07ec20ae7d5e64
Tim Grube <tim2307@gmail.com>
2013-07-08 21:44:58 +0200

    SamplingController.applicable is checking the availability of the necessary submodules. transform method is implemented

--- src/gtna/transformation/sampling/SamplingController.java
+++ src/gtna/transformation/sampling/SamplingController.java
@@ -51,134 +51,151 @@ import gtna.util.parameter.Parameter;
  */
 public class SamplingController extends Transformation {
 
-	AWalkerController walkerController;
-	ASampler sampler;
-	StartNodeSelector startNodeSelector;
-	NetworkSample networkSample;
+    AWalkerController walkerController;
+    ASampler sampler;
+    StartNodeSelector startNodeSelector;
+    NetworkSample networkSample;
 
-	int dimension;
-	double scaledown;
-	boolean revisiting;
+    int dimension;
+    double scaledown;
+    boolean revisiting;
 
-	public SamplingController(String algorithm, AWalkerController awc,
-			ASampler as, StartNodeSelector asns, double scaledown,
-			int dimension, boolean revisiting) {
-		super("SAMPLING_" + algorithm + "_" + awc.getKey() + "_" + as.getKey(),
-				new Parameter[] { awc, as, asns,
-						new DoubleParameter("SCALEDOWN", scaledown),
-						new IntParameter("DIMENSIONS", dimension),
-						new BooleanParameter("REVISITING", revisiting) });
+    public SamplingController(String algorithm, AWalkerController awc,
+	    ASampler as, StartNodeSelector asns, double scaledown,
+	    int dimension, boolean revisiting) {
+	super("SAMPLING_" + algorithm + "_" + awc.getKey() + "_" + as.getKey(),
+		new Parameter[] { awc, as, asns,
+			new DoubleParameter("SCALEDOWN", scaledown),
+			new IntParameter("DIMENSIONS", dimension),
+			new BooleanParameter("REVISITING", revisiting) });
 
-		// we must have at least 1 walker
-		if (dimension < 1) {
-			throw new IllegalArgumentException(
-					"Dimension has to be at least 1 but is: " + dimension);
-		}
-
-		// a scaledown has to be a float between 0.0 and 1.0 representing the
-		// percentage of the original size
-		if (scaledown < 0.0 || scaledown > 1.0) {
-			throw new IllegalArgumentException(
-					"Scaledown has to be in [0,1] but is: " + scaledown);
-		}
-
-		this.scaledown = scaledown;
-		this.dimension = dimension;
-		this.revisiting = revisiting;
-
+	// we must have at least 1 walker
+	if (dimension < 1) {
+	    throw new IllegalArgumentException(
+		    "Dimension has to be at least 1 but is: " + dimension);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see gtna.transformation.Transformation#transform(gtna.graph.Graph)
-	 */
-	@Override
-	public Graph transform(Graph g) {
-		// TODO Auto-generated method stub
-		return null;
+	// a scaledown has to be a float between 0.0 and 1.0 representing the
+	// percentage of the original size
+	if (scaledown < 0.0 || scaledown > 1.0) {
+	    throw new IllegalArgumentException(
+		    "Scaledown has to be in [0,1] but is: " + scaledown);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see gtna.transformation.Transformation#applicable(gtna.graph.Graph)
-	 */
-	@Override
-	public boolean applicable(Graph g) {
-		// do not sample twice with the same algorithm
-		if (g.hasProperty(this.key))
-			return false;
-		else
-			return true;
+	this.scaledown = scaledown;
+	this.dimension = dimension;
+	this.revisiting = revisiting;
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gtna.transformation.Transformation#transform(gtna.graph.Graph)
+     */
+    @Override
+    public Graph transform(Graph g) {
+	if (!initialized()) {
+	    return g;
+	} else {
+	    sampleGraph(g);
+	    return g;
+	}
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see gtna.transformation.Transformation#applicable(gtna.graph.Graph)
+     */
+    @Override
+    public boolean applicable(Graph g) {
+	if (!initialized()) {
+	    return false;
+	}
+	return true;
+    }
+
+    private boolean initialized() {
+	if (dimension <= 0 || networkSample == null || walkerController == null
+		|| !walkerController.isInitialized() || sampler == null
+		|| !sampler.isInitialized() || startNodeSelector == null) {
+	    return false;
 	}
 
-	public int calculateTargetedSampleSize(Graph g, double scaledown) {
+	return true;
+    }
 
-		int originalSize = g.getNodeCount();
+    public int calculateTargetedSampleSize(Graph g, double scaledown) {
 
-		return (int) Math.ceil(originalSize * scaledown);
+	int originalSize = g.getNodeCount();
+
+	return (int) Math.ceil(originalSize * scaledown);
+    }
+
+    public boolean sampleGraph(Graph g) {
+	Node[] startNodes = startNodeSelector.selectStartNodes(g, dimension);
+
+	int targetSampleSize = (int) Math.ceil(g.getNodeCount() * scaledown);
+	int maxNodesInThisRound = calculateResidualBudget(targetSampleSize);
+	int round = 0;
+	walkerController.initialize(g, startNodes); // place walker(s) on start
+						    // node(s)
+	sampler.initialize(walkerController, targetSampleSize); // initialize
+								// Sampler
+
+	// eventually sample startnodes
+	sampleOneStep(maxNodesInThisRound, round);
+
+	boolean running = true;
+	// walk -> sample loop as long new nodes are sampled
+	do {
+	    round++;
+	    // walk
+	    walkerController.walkOneStep();
+	    // eventually sample
+	    sampleOneStep(maxNodesInThisRound, round);
+	    maxNodesInThisRound = calculateResidualBudget(targetSampleSize);
+	    running = (maxNodesInThisRound > 0) ? true : false;
+	} while (running);
+
+	return true;
+    }
+
+    /**
+     * Sample eventually nodes in the specified round
+     * 
+     * @param maxNodesInThisRound
+     *            number of maximal added nodes
+     * @param round
+     *            current round
+     * @return true if at least one node is sampled, else false
+     */
+    private boolean sampleOneStep(int maxNodesInThisRound, int round) {
+	Collection<Node> chosenNodes = sampler
+		.sampleOneStepNodes(maxNodesInThisRound);
+	if (chosenNodes.size() > 0)
+	    return networkSample.addNodeToSample(chosenNodes, round);
+
+	return false;
+    }
+
+    /**
+     * Calculates the residual sampling budget based on the targeted size and
+     * the current size of the sample
+     * 
+     * @param targetSampleSize
+     *            target size
+     * @return residual Budget
+     */
+    private int calculateResidualBudget(int targetSampleSize) {
+	int currentSampleSize = networkSample.getSampleSize();
+	int residualBudget = targetSampleSize - currentSampleSize;
+	if (residualBudget > 0) {
+	    return residualBudget;
+	} else {
+	    return 0;
 	}
-
-	public boolean sampleGraph(Graph g, int targetSampleSize) {
-		Node[] startNodes = startNodeSelector.selectStartNodes(g, dimension);
-
-		int maxNodesInThisRound = calculateResidualBudget(targetSampleSize);
-		int round = 0;
-		walkerController.initialize(g, startNodes); // place walker(s) on start
-													// node(s)
-		sampler.initialize(walkerController, targetSampleSize); // initialize
-																// Sampler
-
-		// eventually sample startnodes
-		sampleOneStep(maxNodesInThisRound, round);
-
-		boolean running = true;
-		// walk -> sample loop as long new nodes are sampled
-		do {
-			round++;
-			// walk
-			walkerController.walkOneStep();
-			// eventually sample
-			sampleOneStep(maxNodesInThisRound, round);
-			maxNodesInThisRound = calculateResidualBudget(targetSampleSize);
-			running = (maxNodesInThisRound > 0) ? true : false;
-		} while (running);
-
-		return true;
-	}
-
-	/**
-	 * Sample eventually nodes in the specified round
-	 * @param maxNodesInThisRound	number of maximal added nodes
-	 * @param round					current round
-	 * @return						true if at least one node is sampled, else false
-	 */
-	private boolean sampleOneStep(int maxNodesInThisRound, int round) {
-		Collection<Node> chosenNodes = sampler
-				.sampleOneStepNodes(maxNodesInThisRound);
-		if (chosenNodes.size() > 0)
-			return networkSample.addNodeToSample(chosenNodes, round);
-
-		return false;
-	}
-
-	/**
-	 * Calculates the residual sampling budget based on the targeted size and
-	 * the current size of the sample
-	 * 
-	 * @param targetSampleSize
-	 *            target size
-	 * @return residual Budget
-	 */
-	private int calculateResidualBudget(int targetSampleSize) {
-		int currentSampleSize = networkSample.getSampleSize();
-		int residualBudget = targetSampleSize - currentSampleSize;
-		if (residualBudget > 0) {
-			return residualBudget;
-		} else {
-			return 0;
-		}
-	}
+    }
 
 }
