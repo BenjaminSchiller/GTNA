236dfcef19e74b729342bcfc518cc3f6b4cde278
Tim Grube <tim2307@gmail.com>
2013-07-12 15:08:35 +0200

    testing & bugfixing (1)

--- src/gtna/transformation/sampling/walkercontroller/UniformSamplingWalkerController.java
+++ src/gtna/transformation/sampling/walkercontroller/UniformSamplingWalkerController.java
@@ -105,6 +105,7 @@ public class UniformSamplingWalkerController extends AWalkerController {
 	Collection<Node> f;
 	for(Node n : candidates.keySet()) {
 	    f = this.filterCandidates(candidates.get(n), ns);
+	    filtered.put(n, f);
 	}
 	
 	
