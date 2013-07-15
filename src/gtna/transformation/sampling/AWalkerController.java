432f9bad74b2e50f8fee8a72dc557b771452e872
Tim Grube <tim2307@googlemail.com>
2013-07-15 19:51:37 +0200

    code format pull method filterCandidates(map<node, collection<node>>) to super class awalkercontroller

--- src/gtna/transformation/sampling/AWalkerController.java
+++ src/gtna/transformation/sampling/AWalkerController.java
@@ -37,6 +37,7 @@ package gtna.transformation.sampling;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import gtna.graph.Graph;
@@ -49,168 +50,182 @@ import gtna.util.parameter.Parameter;
  */
 public abstract class AWalkerController extends Parameter {
 
-    Collection<AWalker> walkers;
-    CandidateFilter candidateFilter;
-    private SamplingController samplingController;
+	CandidateFilter candidateFilter;
+	private SamplingController samplingController;
+	Collection<AWalker> walkers;
 
-    /**
-     * Instantiate the walker controller
-     * 
-     * @param value
-     */
-    public AWalkerController(String walkercontroller) {
-	super("WALKER_CONTROLLER", walkercontroller);
+	/**
+	 * Instantiate the walker controller
+	 * 
+	 * @param value
+	 */
+	public AWalkerController(String walkercontroller) {
+		super("WALKER_CONTROLLER", walkercontroller);
 
-    }
-
-    /**
-     * Instantiate the walker controller and set necessary components
-     * 
-     * @param walkercontroller
-     * @param w
-     *            Collection of walkers
-     * @param cf
-     *            candidate filter
-     * @param sns
-     *            start node selector
-     */
-    public AWalkerController(String walkercontroller, Collection<AWalker> w,
-	    CandidateFilter cf) {
-	super("WALKER_CONTROLLER", walkercontroller);
-	this.walkers = w;
-	this.candidateFilter = cf;
-    }
-
-    /**
-     * @param g
-     * @param startNodes
-     */
-    public abstract void initialize(Node[] startNodes);
-
-    /**
-     * initialize the walker controller
-     * 
-     * @param g
-     *            graph
-     * @param startNodes
-     *            start nodes for the walker instances
-     * @param w
-     *            collection of walkers
-     * @param cf
-     *            candidate filter
-     */
-    public void initialize(Node[] startNodes, Collection<AWalker> w,
-	    CandidateFilter cf) {
-	walkers = w;
-	candidateFilter = cf;
-	this.initialize(startNodes);
-    }
-
-    /**
-     * Checks the initialization of the walker controller
-     * 
-     * @return true if ok, else false
-     */
-    public boolean isInitialized() {
-	if (walkers == null || walkers.size() == 0 || candidateFilter == null || samplingController == null) {
-	    return false;
-	} else {
-	    return true;
-	}
-    }
-
-    /**
-     * Perform one step of walking with all active walker. The number of active
-     * walkers depends on the used walking-strategy
-     */
-    public void walkOneStep() {
-	if (!isInitialized()) {
-	    throw new IllegalStateException(
-		    "You have to initialize the WalkerController first.");
 	}
 
-	Collection<AWalker> activeWalkers = this.getActiveWalkers();
-	for (AWalker w : activeWalkers) {
-	    w.takeAStep(this.getGraph(), this.getNetworkSample());
+	/**
+	 * Instantiate the walker controller and set necessary components
+	 * 
+	 * @param walkercontroller
+	 * @param w
+	 *            Collection of walkers
+	 * @param cf
+	 *            candidate filter
+	 * @param sns
+	 *            start node selector
+	 */
+	public AWalkerController(String walkercontroller, Collection<AWalker> w,
+			CandidateFilter cf) {
+		super("WALKER_CONTROLLER", walkercontroller);
+		this.walkers = w;
+		this.candidateFilter = cf;
 	}
-    }
-    
-    /**
-     * returns the current network sample hold by the sampling controller
-     * @return
-     */
-    public NetworkSample getNetworkSample() {
-	return getSamplingController().getNetworkSample();
-    }
 
-    /**
-     * evaluates the collection of walkers to return a subset of the walkers
-     * which have to take a step
-     */
-    protected abstract Collection<AWalker> getActiveWalkers();
+	/**
+	 * Filters the list of candidates for real candidates e.g. without already
+	 * sampled nodes
+	 * 
+	 * @param candidates
+	 *            possible nodes
+	 * @param sample
+	 *            current networksamples
+	 * @return subset of candidates
+	 */
+	public abstract Collection<Node> filterCandidates(
+			Collection<Node> candidates);
 
-    /**
-     * Filters the list of candidates for real candidates e.g. without already
-     * sampled nodes
-     * 
-     * @param candidates
-     *            possible nodes
-     * @param sample
-     *            current networksamples
-     * @return subset of candidates
-     */
-    public abstract Map<Node, Collection<Node>> filterCandidates(
-	    Map<Node, Collection<Node>> candidates);
+	
+	/**
+	 * Filters the list of candidates for real candidates e.g. without already
+	 * sampled nodes
+	 * 
+	 * @param candidates
+	 *            possible nodes
+	 * @param sample
+	 *            current networksamples
+	 * @return subset of candidates
+	 */
+	public Map<Node, Collection<Node>> filterCandidates(
+			Map<Node, Collection<Node>> candidates) {
+		Map<Node, Collection<Node>> filtered = new HashMap<Node, Collection<Node>>();
+		Collection<Node> f;
+		for (Node n : candidates.keySet()) {
+			f = this.filterCandidates(candidates.get(n));
+			filtered.put(n, f);
+		}
 
-    /**
-     * Filters the list of candidates for real candidates e.g. without already
-     * sampled nodes
-     * 
-     * @param candidates
-     *            possible nodes
-     * @param sample
-     *            current networksamples
-     * @return subset of candidates
-     */
-    public abstract Collection<Node> filterCandidates(
-	    Collection<Node> candidates);
+		return filtered;
+	}
 
-    /**
-     * get new start nodes for restarting a walker instance
-     * 
-     * @return collection of new start nodes
-     */
-    public Collection<Node> getRestartNodes() {
-	Node[] rn;
-	Collection<Node> frn, c;
-	do {
-	    rn = samplingController.getStartNodeSelector().selectStartNodes(this.getGraph(), 1);
-	    c = Arrays.asList(rn);
-	    frn = filterCandidates(c);
-	} while (frn.size() == 0);
+	/**
+	 * evaluates the collection of walkers to return a subset of the walkers
+	 * which have to take a step
+	 */
+	protected abstract Collection<AWalker> getActiveWalkers();
 
-	return frn;
-    }
+	/**
+	 * @return
+	 */
+	public Graph getGraph() {
+		return samplingController.getGraph();
+	}
 
-    /**
-     * @return
-     */
-    public Graph getGraph() {
-	return samplingController.getGraph();
-    }
+	/**
+	 * returns the current network sample hold by the sampling controller
+	 * 
+	 * @return
+	 */
+	public NetworkSample getNetworkSample() {
+		return getSamplingController().getNetworkSample();
+	}
 
-    /**
-     * @return the samplingController
-     */
-    public SamplingController getSamplingController() {
-	return samplingController;
-    }
+	/**
+	 * get new start nodes for restarting a walker instance
+	 * 
+	 * @return collection of new start nodes
+	 */
+	public Collection<Node> getRestartNodes() {
+		Node[] rn;
+		Collection<Node> frn, c;
+		do {
+			rn = samplingController.getStartNodeSelector().selectStartNodes(
+					this.getGraph(), 1);
+			c = Arrays.asList(rn);
+			frn = filterCandidates(c);
+		} while (frn.size() == 0);
 
-    /**
-     * @param samplingController the samplingController to set
-     */
-    public void setSamplingController(SamplingController samplingController) {
-	this.samplingController = samplingController;
-    }
+		return frn;
+	}
+
+	/**
+	 * @return the samplingController
+	 */
+	public SamplingController getSamplingController() {
+		return samplingController;
+	}
+
+	/**
+	 * @param g
+	 * @param startNodes
+	 */
+	public abstract void initialize(Node[] startNodes);
+
+	/**
+	 * initialize the walker controller
+	 * 
+	 * @param g
+	 *            graph
+	 * @param startNodes
+	 *            start nodes for the walker instances
+	 * @param w
+	 *            collection of walkers
+	 * @param cf
+	 *            candidate filter
+	 */
+	public void initialize(Node[] startNodes, Collection<AWalker> w,
+			CandidateFilter cf) {
+		walkers = w;
+		candidateFilter = cf;
+		this.initialize(startNodes);
+	}
+
+	/**
+	 * Checks the initialization of the walker controller
+	 * 
+	 * @return true if ok, else false
+	 */
+	public boolean isInitialized() {
+		if (walkers == null || walkers.size() == 0 || candidateFilter == null
+				|| samplingController == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * @param samplingController
+	 *            the samplingController to set
+	 */
+	public void setSamplingController(SamplingController samplingController) {
+		this.samplingController = samplingController;
+	}
+
+	/**
+	 * Perform one step of walking with all active walker. The number of active
+	 * walkers depends on the used walking-strategy
+	 */
+	public void walkOneStep() {
+		if (!isInitialized()) {
+			throw new IllegalStateException(
+					"You have to initialize the WalkerController first.");
+		}
+
+		Collection<AWalker> activeWalkers = this.getActiveWalkers();
+		for (AWalker w : activeWalkers) {
+			w.takeAStep(this.getGraph(), this.getNetworkSample());
+		}
+	}
 
 }
