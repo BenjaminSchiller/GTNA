b91292b575630fc860193d5441e34dc3d4c255dd
Tim Grube <tim2307@googlemail.com>
2013-07-15 19:52:39 +0200

    added RandomWalkWalker

---
+++ src/gtna/transformation/sampling/walker/RandomWalkWalker.java
@@ -0,0 +1,70 @@
+/* ===========================================================
+ * GTNA : Graph-Theoretic Network Analyzer
+ * ===========================================================
+ *
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors
+ *
+ * Project Info:  http://www.p2p.tu-darmstadt.de/research/gtna/
+ *
+ * GTNA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GTNA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ---------------------------------------
+ * RandomWalkWalker.java
+ * ---------------------------------------
+ * (C) Copyright 2009-2011, by Benjamin Schiller (P2P, TU Darmstadt)
+ * and Contributors 
+ *
+ * Original Author: Tim;
+ * Contributors:    -;
+ *
+ * Changes since 2011-05-17
+ * ---------------------------------------
+ *
+ */
+package gtna.transformation.sampling.walker;
+
+import java.util.Collection;
+import java.util.Random;
+
+import gtna.graph.Node;
+import gtna.transformation.sampling.AWalker;
+
+/**
+ * @author Tim
+ *
+ */
+public class RandomWalkWalker extends AWalker {
+
+	/**
+	 * @param walker
+	 */
+	public RandomWalkWalker(String walker) {
+		super(walker);
+	}
+
+	/* (non-Javadoc)
+	 * @see gtna.transformation.sampling.AWalker#selectNextNode(java.util.Collection)
+	 */
+	@Override
+	protected Node selectNextNode(Collection<Node> candidates) {
+		Random r = new Random();
+		
+		int next = r.nextInt(candidates.size());
+		next = next % candidates.size();
+		
+		return candidates.toArray(new Node[0])[next];
+	}
+
+}
