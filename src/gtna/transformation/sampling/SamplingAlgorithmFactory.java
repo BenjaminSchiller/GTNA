94aac6318f8a3e430d4e6fa553b6760367e0e490
Tim Grube <tim2307@googlemail.com>
2013-08-17 23:37:03 +0200

    added ForestFireWalker

--- src/gtna/transformation/sampling/SamplingAlgorithmFactory.java
+++ src/gtna/transformation/sampling/SamplingAlgorithmFactory.java
@@ -41,6 +41,7 @@ import java.util.Collection;
 import gtna.transformation.sampling.sampler.RoundBasedVisitedNodeSampler;
 import gtna.transformation.sampling.sampler.VisitedNodeSampler;
 import gtna.transformation.sampling.walker.BFSWalker;
+import gtna.transformation.sampling.walker.FFWalker;
 import gtna.transformation.sampling.walker.RDSWalker;
 import gtna.transformation.sampling.walker.RandomJumpWalker;
 import gtna.transformation.sampling.walker.RandomWalkDegreeCorrectionWalker;
@@ -274,7 +275,7 @@ public class SamplingAlgorithmFactory {
 			aw.setWalkerController(awc);
 			as.setWalkerController(awc);
 			
-			algorithm = "RESPONDENT_DRIVEN_SAMPLING";
+			algorithm = "FOREST_FIRE_SAMPLING";
 			break;
 		default:
 			throw new IllegalArgumentException("Not supported algorithm");
