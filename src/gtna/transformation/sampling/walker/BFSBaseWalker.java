290e5d1c171149f585903495981e2c178930be47
Tim Grube <tim2307@googlemail.com>
2013-08-18 00:02:40 +0200

    added a BaseWalker for BFS-like Sampling algorithms as the differences between BFS, FF, RDS and SB are very small

--- src/gtna/transformation/sampling/walker/BFSBaseWalker.java
+++ src/gtna/transformation/sampling/walker/BFSBaseWalker.java
@@ -37,7 +37,10 @@ package gtna.transformation.sampling.walker;
 
 import java.util.ArrayList;
 import java.util.Collection;
+<<<<<<< HEAD
 import java.util.Deque;
+=======
+>>>>>>> added a BaseWalker for BFS-like Sampling algorithms as the differences between BFS, FF, RDS and SB are very small
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -47,11 +50,16 @@ import java.util.Random;
 import gtna.graph.Graph;
 import gtna.graph.Node;
 import gtna.transformation.sampling.AWalker;
+<<<<<<< HEAD
 import gtna.transformation.sampling.sample.NetworkSample;
+=======
+import gtna.transformation.sampling.NetworkSample;
+>>>>>>> added a BaseWalker for BFS-like Sampling algorithms as the differences between BFS, FF, RDS and SB are very small
 
 /**
  * @author Tim
  * 
+<<<<<<< HEAD
  * This class provides the base functionality of a bfs/dfs walker using a deque datastructure
  * 
  */
@@ -146,5 +154,99 @@ public abstract class BFSBaseWalker extends AWalker {
 		}
 		return nn;
 	}
+=======
+ */
+public abstract class BFSBaseWalker extends AWalker {
+
+    List<Node> nextQ;
+    private int restartcounter = 0;
+
+    /**
+     * @param walker
+     */
+    public BFSBaseWalker(String key) {
+    	super(key);
+    	nextQ = new LinkedList<Node>();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * gtna.transformation.sampling.AWalker#selectNextNode(java.util.Collection)
+     */
+    @Override
+    protected Node selectNextNode(Collection<Node> candidates) {
+	Node n = null;
+	List<Node> c = new ArrayList<Node>();
+	Collection<Node> cc = new ArrayList<Node>();
+	while (n == null) {
+	    if (nextQ.size() > 0) {
+		c.add(nextQ.get(0));
+		nextQ.remove(0);
+		cc = this.filterCandidates(c);
+		if (cc.size() > 0) {
+		    n = cc.toArray(new Node[0])[0];
+		}
+	    } else {
+		
+		System.err.println("NextQ empty, need a restart! (" + restartcounter  
+			+ ")");
+		restartcounter += 1;
+		cc = super.getRestartNodes();
+		n = cc.toArray(new Node[0])[0];
+		
+	    }
+	}
+
+	return n;
+    }
+
+    @Override
+    public void takeAStep(Graph g, NetworkSample ns) {
+	Map<Node, Collection<Node>> cc = this.getCurrentCandidates();
+	Collection<Node> c = new ArrayList<Node>();
+
+	// add new neighbors to the q
+	if (cc.size() > 0) {
+	    c = cc.keySet();
+	}
+	
+	    Collection<Collection<Node>> toQ = cc.values();
+	    for(Collection<Node> cn : toQ) {
+	    	nextQ.addAll(chooseNodesToAddToQ(cn));
+	    }
+	    
+	    Node next = this.selectNextNode(new ArrayList<Node>());
+	    
+	    
+	    super.currents.remove(cc.keySet().toArray(new Node[0])[0]);
+	    super.currents.add(next);
+
+	
+
+    }
+    
+    protected abstract Collection<Node> chooseNodesToAddToQ(Collection<Node> toFilter);
+
+    /**
+     * returns the list of neighbors as candidates
+     * 
+     * @param g
+     *            Graph
+     * @param n
+     *            Current node
+     * @return List of candidates
+     */
+    @Override
+    public Collection<Node> resolveCandidates(Graph g, Node n) {
+	int[] nids = n.getOutgoingEdges();
+	ArrayList<Node> nn = new ArrayList<Node>();
+	for (int i : nids) {
+	    nn.add(g.getNode(i));
+	}
+	return nn;
+    }
+>>>>>>> added a BaseWalker for BFS-like Sampling algorithms as the differences between BFS, FF, RDS and SB are very small
 
 }
