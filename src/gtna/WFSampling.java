7e36206f55387cc6b7084ca1d285592547c1eec9
Tim Grube <tim2307@gmail.com>
2013-08-31 22:17:31 +0200

    Correct parameter for subgraph transformations Functionality of WFSampling App for loading networks, applying a sampling transformation followed by subgraph transformation and persist the sampled graph

--- src/gtna/WFSampling.java
+++ src/gtna/WFSampling.java
@@ -58,188 +58,197 @@ import java.util.Date;
  */
 public class WFSampling {
 
-	private static Transformation subgraph;
-	private static SamplingAlgorithm samplingAlgorithm;
-	private static double scaledown;
-	private static boolean rev;
-	private static int dim;
-	private static long seed;
-	private static String dir;
-	private static int startIndex;
-	private static int endIndex;
-	private static String srcdir;
-	private static String suffix;
-	private static String targetdir;
+    private static Transformation subgraph;
+    private static SamplingAlgorithm samplingAlgorithm;
+    private static Double scaledown;
+    private static boolean rev = false;
+    private static Integer dim;
+    private static Long seed;
+    private static String dir;
+    private static Integer startIndex;
+    private static Integer endIndex;
+    private static String srcdir;
+    private static String suffix = "";
+    private static String targetdir;
 
-	/**
-	 * @param args
-	 * @throws ParseException 
-	 */
-	public static void main(String[] args) throws ParseException {
-		
-		
-	    if (args.length == 0
-			|| (args.length == 1 && args[0].equalsIgnoreCase("help"))) {
-		    printHelp();
-		    System.exit(0);
-		}
+    /**
+     * @param args
+     * @throws ParseException
+     */
+    public static void main(String[] args) throws ParseException {
 
-		for (String s : args) {
-		    matchArgument(s);
-		}
-		if(!initialized()) {
-		    System.out.println("You have to set all necessary params first!");
-		}
-		
-		Transformation[] samplingTransformation = new Transformation[2];
-		samplingTransformation[0] = instantiateSamplingTransformation(samplingAlgorithm, scaledown, dim, rev, seed);
-		if(subgraph != null) {
-		    samplingTransformation[1] = subgraph;
-		}
-		
-		ReadableFolder rf = new ReadableFolder(samplingAlgorithm.name(), dir, srcdir, suffix, samplingTransformation); 
-		// current index is 0!
-		if(startIndex > 0) {
-		    for(int i = 0; i < startIndex; i++) {
-			rf.incIndex();
-		    }
-		}
-		
-		Graph g;
-		for(int j = startIndex; j < endIndex; j++) {
-		    g = rf.generate();
-		    if(g!=null) {
-		    g = samplingTransformation[0].transform(g); // sample graph
-		    g = samplingTransformation[1].transform(g);	// subgraph generation/coloring
-		    
-		    writeGraphToFile(g, targetdir+g.getName());
-		    }
-		}
-
+	if (args.length == 0
+		|| (args.length == 1 && args[0].equalsIgnoreCase("help"))) {
+	    printHelp();
+	    System.exit(1);
 	}
 
-	/**
-	 * @return
-	 */
-	private static boolean initialized() {
-	    // TODO Auto-generated method stub
-	    return true;
+	for (String s : args) {
+	    matchArgument(s);
+	}
+	if (!initialized()) {
+	    System.out.println("You have to set all necessary params first!");
+	    System.exit(1);
 	}
 
-	private static void printHelp() {
-		System.out.println("Usage:"
-				+ "sampling=<samplingalgorithm> dimension=<how many walker> scaledown=<sample size in percentage> revisiting=<true/false> randomSeed=<dd.mm.yyyy>"
-				+ "loadDir=<directory with prepared networks>");
+	Transformation[] samplingTransformation = new Transformation[2];
+	samplingTransformation[0] = instantiateSamplingTransformation(
+		samplingAlgorithm, scaledown, dim, rev, seed);
+	if (subgraph != null) {
+	    samplingTransformation[1] = subgraph;
 	}
 
-	private static Transformation instantiateSamplingTransformation(
-			SamplingAlgorithm alg, double sd, int dimension,
-			boolean revisiting, Long randomSeed) {
-		Transformation samplingalgorithm = SamplingAlgorithmFactory
-				.getInstanceOf(alg, sd, revisiting, dimension,
-						randomSeed);
-		return samplingalgorithm;
-	}
-	
-	
-	/**
-	     * @param s
-	     * @throws ParseException
-	     */
-	    private static void matchArgument(String s) throws ParseException {
-
-		// parse network generation details
-		if (s.startsWith("sampling=")) {
-		    String sn = s.substring(9);
-		    samplingAlgorithm = matchSamplingAlgorithm(sn);
-		} else if (s.startsWith("scaledown=")) {
-		    scaledown = Double.parseDouble(s.substring(10));
-		} else if (s.startsWith("rev=")) {
-		    if(s.equalsIgnoreCase("rev=true")) {
-			rev = true;
-		    }else {
-			rev = false;
-		    }
-		} else if (s.startsWith("dim=")) {
-		    dim = Integer.parseInt(s.substring(4));
-		}else if (s.startsWith("suffix=")) {
-		    suffix = s.substring(7);
-		}  
-		else if (s.startsWith("subgraph=")) {
-		    String sg = s.substring(9);
-		    subgraph = matchSubgraph(sg);
-		}  else if(s.startsWith("randomSeed=")){
-			String seedDate = s.substring(11);
-			seedDate.matches("[0-3][0-9]\\.[0-1][0-9]\\.[0-9][0-9][0-9][0-9]");
-			DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-			Date seeddate =  df.parse(seedDate);
-			seed = seeddate.getTime();
-		} 
-		else if (s.startsWith("dir=")) {
-		    dir = s.substring(4);
-		    File f = new File(dir);
-		    if (!f.isDirectory()) {
-			f.mkdir();
-		    }
-		} else if (s.startsWith("srcdir=")) {
-		    srcdir = s.substring(7);
-		    File f = new File(srcdir);
-		    if (!f.isDirectory()) {
-			f.mkdir();
-		    }
-		}else if (s.startsWith("targetdir=")) {
-		    targetdir = s.substring(10);
-		    File f = new File(targetdir);
-		    if (!f.isDirectory()) {
-			f.mkdir();
-		    }
-		} else if (s.startsWith("seq=")) {
-		    String seq = s.substring(4);
-		    String[] se = seq.split("-");
-		    startIndex = Integer.parseInt(se[0]);
-		    endIndex = Integer.parseInt(se[1]);
-		} 
-		
+	ReadableFolder rf = new ReadableFolder(samplingAlgorithm.name(), dir,
+		srcdir, suffix, null);
+	// current index is 0!
+	if (startIndex > 0) {
+	    for (int i = 0; i < startIndex; i++) {
+		rf.incIndex();
+	    }
 	}
 
-	    
-	private static String writeGraphToFile(Graph g, String filename) {
-		new GtnaGraphWriter().writeWithProperties(g, filename);
-		return filename;
-	}
-	    
-	/**
-	 * @param sn
-	 * @return
-	 */
-	private static SamplingAlgorithm matchSamplingAlgorithm(String sn) {
-	   for(SamplingAlgorithm sa : SamplingAlgorithm.values()) {
-	       if(sn.equalsIgnoreCase(sa.name())) {
-		   return sa;
-	       }
-	   }
-	   
-	   throw new IllegalArgumentException("Sampling Algorithm unknown, please choose one of: " + Arrays.toString(SamplingAlgorithm.values()));    
+	Graph g;
+	for (int j = startIndex; j < endIndex; j++) {
+	    g = rf.generate();
+	    if (g != null) {
+		g = samplingTransformation[0].transform(g); // sample graph
+		g = samplingTransformation[1].transform(g); // subgraph
+							    // generation/coloring
+
+		writeGraphToFile(g, targetdir + g.getName() + "_" + j);
+	    }
 	}
 
-	/**
-	 * @param sg
-	 * @return
-	 */
-	private static Transformation matchSubgraph(String sg) {
-	    Transformation subgraph;
-		if (sg.equalsIgnoreCase("subgraph")) {
-		    subgraph = new ExtractSampledSubgraph();
-		} else if (sg.equalsIgnoreCase("coloring")) {
-		    subgraph = new ColorSampledSubgraph();
-		} else if (sg.equalsIgnoreCase("heatmap")) {
-		    subgraph = new ColoredHeatmapSampledSubgraph();
-		} else {
-			throw new IllegalArgumentException(
-					"subgraph must be one of {subgraph, coloring, heatmap}");
-		}
-		
-		return subgraph;
-	} 
+    }
+
+    /**
+     * @return
+     */
+    private static boolean initialized() {
+
+	if (dim == null || dim > 1 || dir == null ||
+		srcdir == null || 
+		targetdir == null||  
+		scaledown == null) {
+	    return false;
+	}
+
+	return true;
+
+    }
+
+    private static void printHelp() {
+	System.out
+		.println("Usage:"
+			+ "sampling=<samplingalgorithm> dimension=<how many walker> scaledown=<sample size in percentage> revisiting=<true/false> randomSeed=<dd.mm.yyyy>"
+			+ "loadDir=<directory with prepared networks>");
+    }
+
+    private static Transformation instantiateSamplingTransformation(
+	    SamplingAlgorithm alg, double sd, int dimension,
+	    boolean revisiting, Long randomSeed) {
+	Transformation samplingalgorithm = SamplingAlgorithmFactory
+		.getInstanceOf(alg, sd, revisiting, dimension, randomSeed);
+	return samplingalgorithm;
+    }
+
+    /**
+     * @param s
+     * @throws ParseException
+     */
+    private static void matchArgument(String s) throws ParseException {
+
+	// parse network generation details
+	if (s.startsWith("sampling=")) {
+	    String sn = s.substring(9);
+	    samplingAlgorithm = matchSamplingAlgorithm(sn);
+	} else if (s.startsWith("scaledown=")) {
+	    scaledown = Double.parseDouble(s.substring(10));
+	} else if (s.startsWith("rev=")) {
+	    if (s.equalsIgnoreCase("rev=true")) {
+		rev = true;
+	    } else {
+		rev = false;
+	    }
+	} else if (s.startsWith("dim=")) {
+	    dim = Integer.parseInt(s.substring(4));
+	} else if (s.startsWith("suffix=")) {
+	    suffix = s.substring(7);
+	} else if (s.startsWith("subgraph=")) {
+	    String sg = s.substring(9);
+	    subgraph = matchSubgraph(sg);
+	} else if (s.startsWith("randomSeed=")) {
+	    String seedDate = s.substring(11);
+	    seedDate.matches("[0-3][0-9]\\.[0-1][0-9]\\.[0-9][0-9][0-9][0-9]");
+	    DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+	    Date seeddate = df.parse(seedDate);
+	    seed = seeddate.getTime();
+	} else if (s.startsWith("dir=")) {
+	    dir = s.substring(4);
+	    File f = new File(dir);
+	    if (!f.isDirectory()) {
+		f.mkdir();
+	    }
+	} else if (s.startsWith("srcdir=")) {
+	    srcdir = s.substring(7);
+	    File f = new File(srcdir);
+	    if (!f.isDirectory()) {
+		f.mkdir();
+	    }
+	} else if (s.startsWith("targetdir=")) {
+	    targetdir = s.substring(10);
+	    File f = new File(targetdir);
+	    if (!f.isDirectory()) {
+		f.mkdir();
+	    }
+	} else if (s.startsWith("seq=")) {
+	    String seq = s.substring(4);
+	    String[] se = seq.split("-");
+	    startIndex = Integer.parseInt(se[0]);
+	    endIndex = Integer.parseInt(se[1]);
+	}
+
+    }
+
+    private static String writeGraphToFile(Graph g, String filename) {
+	new GtnaGraphWriter().writeWithProperties(g, filename);
+	return filename;
+    }
+
+    /**
+     * @param sn
+     * @return
+     */
+    private static SamplingAlgorithm matchSamplingAlgorithm(String sn) {
+	for (SamplingAlgorithm sa : SamplingAlgorithm.values()) {
+	    if (sn.equalsIgnoreCase(sa.name())) {
+		return sa;
+	    }
+	}
+
+	throw new IllegalArgumentException(
+		"Sampling Algorithm unknown, please choose one of: "
+			+ Arrays.toString(SamplingAlgorithm.values()));
+    }
+
+    /**
+     * @param sg
+     * @return
+     */
+    private static Transformation matchSubgraph(String sg) {
+	Transformation subgraph;
+	if (sg.equalsIgnoreCase("subgraph")) {
+	    subgraph = new ExtractSampledSubgraph();
+	} else if (sg.equalsIgnoreCase("coloring")) {
+	    subgraph = new ColorSampledSubgraph();
+	} else if (sg.equalsIgnoreCase("heatmap")) {
+	    subgraph = new ColoredHeatmapSampledSubgraph();
+	} else {
+	    throw new IllegalArgumentException(
+		    "subgraph must be one of {subgraph, coloring, heatmap}");
+	}
+
+	return subgraph;
+    }
 
 }
