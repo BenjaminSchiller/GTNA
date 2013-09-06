bac8857acd412544528b9632ce6d83ed6cd93902
Tim Grube <tim2307@gmail.com>
2013-09-07 00:55:22 +0200

    bugfix bc (1)

--- src/gtna/Exploring.java
+++ src/gtna/Exploring.java
@@ -41,18 +41,15 @@ import gtna.graph.Graph;
 import gtna.id.IdentifierSpace;
 import gtna.io.graphWriter.GtnaGraphWriter;
 import gtna.metrics.Metric;
-import gtna.metrics.basic.ClusteringCoefficient;
-import gtna.metrics.basic.DegreeDistribution;
-import gtna.metrics.basic.ShortestPaths;
+import gtna.metrics.centrality.BetweennessCentrality;
 import gtna.networks.Network;
-import gtna.networks.model.ZhouMondragon;
+import gtna.networks.model.Regular;
 import gtna.plot.Plotting;
 import gtna.transformation.Transformation;
 import gtna.transformation.id.ConsecutiveRingIDSpace;
 import gtna.transformation.id.RandomPlaneIDSpaceSimple;
 import gtna.transformation.id.RandomRingIDSpaceSimple;
 import gtna.util.Config;
-import gtna.util.Stats;
 
 /**
  * @author Tim
@@ -69,26 +66,46 @@ public class Exploring {
 		int times = 1;		// how many generations?
 		boolean b = false; // bidirectional
 		
+//		Transformation t = new Bidirectional();
+
+//		Network nw0 = new Regular(10, 2, true, false, null);
+//		Network nw1 = new ErdosRenyi(10, 3, false, null);
+//		Network nw2 = new BarabasiAlbert(10, 2, null);
+//		Network nw3 = new WattsStrogatz(10, 2, 0.2, null);
+//		Network nw4 = new CondonAndKarp(10, 2, 0.4, 0.05, null);
+//		Network nw5 = new Regular(100, 5, true, false, null);
+//		Network nw6 = new ErdosRenyi(100, 10, false, null);
+//		Network nw7 = new BarabasiAlbert(100, 10, null);
+//		Network nw8 = new WattsStrogatz(100, 6, 0.2, null);
+//		Network nw9 = new CondonAndKarp(100, 4, 0.4, 0.05, null);
 		
 
 		boolean r = false;
 		
-		Network nw0 = new ZhouMondragon(10000, 0.25, 2, null);
+		Network nw0 = new Regular(100, 4, r, b, null);
+		Network nw1 = new Regular(200, 4, r, b, null);
+		Network nw2 = new Regular(300, 4, r, b, null);
+		Network nw3 = new Regular(400, 4, r, b, null);
+		Network nw4 = new Regular(500, 4, r, b, null);
+		Network nw5 = new Regular(600, 4, r, b, null);
+		Network nw6 = new Regular(700, 4, r, b, null);
+		Network nw7 = new Regular(800, 4, r, b, null);
+		Network nw8 = new Regular(900, 4, r, b, null);
+		Network nw9 = new Regular(1000, 4, r, b, null);
 		
-		
-		Network[] n = new Network[] {nw0};
+		Network[] n = new Network[] {nw0 /*, nw1, nw2, nw3, nw4, nw5, nw6, nw7, nw8, nw9*/};
 		
 		Metric[] metrics = new Metric[] { 
-				new DegreeDistribution() };
+				new BetweennessCentrality()
+				};
 		
 		
 		Series[] s = get ? Series.get(n, metrics) : Series.generate(n, metrics, times);
-		
+
 		Plotting.single(s, metrics, "example-s/");
-//
+
 		Plotting.multi(s, metrics, "example-m/");
-//		
-		
+		 
 		 for(Network i : n){
 			 System.out.println("Plotting network - " + i.getKey() + " @ " + i.getNodes() + " nodes");
 			 plot(i, "./plots/network-plot/n-"+i.getKey() + "-" + i.getNodes(), times);
