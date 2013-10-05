e3eb851e88b7a3a1ace5d7598013bb33c03bf84f
Tim Grube <tim2307@gmail.com>
2013-10-05 02:46:51 +0200

    fixes: - ExtractSampledSubgraph does not change the original graph! - changes in structure of the WFApps

--- src/gtna/Exploring.java
+++ src/gtna/Exploring.java
@@ -39,10 +39,14 @@ import gtna.drawing.Gephi;
 import gtna.graph.Graph;
 import gtna.id.IdentifierSpace;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import gtna.io.graphWriter.GtnaGraphWriter;
 import gtna.metric.centrality.PageRank;
 =======
 >>>>>>> optimizing the PR metric
+=======
+import gtna.io.graphWriter.GtnaGraphWriter;
+>>>>>>> fixes:
 import gtna.metrics.Metric;
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -142,6 +146,7 @@ import gtna.transformation.sampling.subgraph.ExtractSampledSubgraphWithNeighborS
 import gtna.util.Config;
 import gtna.util.Stats;
 
+import java.io.File;
 import java.util.Arrays;
 
 /**
@@ -155,6 +160,7 @@ public class Exploring {
 		// Config.overwrite("GNUPLOT_PRINT_ERRORS", "true");
 		
 		
+<<<<<<< HEAD
 		boolean get = false; // get or generate
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -204,18 +210,26 @@ public class Exploring {
 >>>>>>> Sampling Modularity implemented
 		
 		SamplingAlgorithm a = SamplingAlgorithm.BFS;
+=======
+//		boolean get = false; // get or generate
+//		int times = 1;		// how many generations?
+//		boolean b = false; // bidirectional?
+//		boolean r = false; // ring?
+//		
+		SamplingAlgorithm a = SamplingAlgorithm.UNIFORMSAMPLING;
+>>>>>>> fixes:
 		double sc = 0.2;
 		
-		Transformation sa = SamplingAlgorithmFactory.getInstanceOf(a, sc, true, 1, new Long(0));
-		Transformation sa2 = SamplingAlgorithmFactory.getInstanceOf(a, sc, true, 1, new Long(0));
-		Transformation[] t = new Transformation[2];
+		Transformation sa = SamplingAlgorithmFactory.getInstanceOf(a, sc, true, 1, null);
+		Transformation sa2 = SamplingAlgorithmFactory.getInstanceOf(a, sc, true, 1, null);
+		Transformation[] t = new Transformation[3];
 		
 		
 		
 		Arrays.fill(t, sa);
 		t[0] = sa;
-//		t[1] = sa2;
-		t[t.length-1] = new ExtractSampledSubgraphWithNeighborSet();
+		t[1] = sa2;
+		t[t.length-1] = new ExtractSampledSubgraph();
 		
 		
 <<<<<<< HEAD
@@ -300,6 +314,7 @@ public class Exploring {
 >>>>>>> Sampling Modularity implemented
 =======
 //		Network nw1 = new Regular(30, 10, true, false, null);
+<<<<<<< HEAD
 		Network nw3 = new Regular(100, 10, true, false, null);
 		Network nw1 = new WattsStrogatz(1000, 10, 0.1, null);
 		Network nw2 = new WattsStrogatz(1000, 10, 0.01, null);
@@ -336,6 +351,29 @@ public class Exploring {
 //				new DegreeDistribution(),
 //				new ClusteringCoefficient(),
 				new ShortestPaths()
+=======
+		Network nw3 = new Regular(1000, 10, true, false, null);
+//		Network nw1 = new WattsStrogatz(1000, 10, 0.1, null);
+//		Network nw2 = new WattsStrogatz(1000, 10, 0.01, null);
+//		
+//		Network[] ws1 = ZhouMondragon.get(50, new double[] {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6}, 7, null);
+//		
+//		Network[] nw = new Network[2];
+//		Arrays.fill(nw, nw1);
+//		Network nw0 = new GeneralizedCondonAndKarp(nw, 0.00005, t);
+//		
+//		Network[] n = new Network[] {nw3};
+////		Network[] n = ws1;
+//		
+//		DegreeDistribution m = new DegreeDistribution();
+//		m.computeData(nw3.generate(), nw3, null);
+//		
+//		
+//		Metric[] metrics = new Metric[] { 
+////				new DegreeDistribution(),
+////				new ClusteringCoefficient(),
+//				new ShortestPaths()
+>>>>>>> fixes:
 //				new BetweennessCentrality(),
 //				new Assortativity(),
 //				new SamplingBias()
@@ -343,6 +381,7 @@ public class Exploring {
 //				new SamplingModularity(),
 //				new DegreeDistributionComparator(m),
 //				new SamplingRevisitFrequency()
+<<<<<<< HEAD
 >>>>>>> optimizing the PR metric
 				};
 =======
@@ -352,6 +391,9 @@ public class Exploring {
 <<<<<<< HEAD
 		
 		Series[] s = get ? Series.get(n, metrics) : Series.generate(n, metrics, times);
+=======
+//				};
+>>>>>>> fixes:
 		
 =======
 //		Series[] s = get ? Series.get(n, metrics) : Series.generate(n, metrics, times);
@@ -421,12 +463,77 @@ public class Exploring {
 >>>>>>> plotting for example plots
 =======
 		
+<<<<<<< HEAD
 		 for(Network i : n){
 			 System.out.println("Plotting network - " + i.getKey() + " @ " + i.getNodes() + " nodes");
 			 plot(i, "./plots/network-plot/"+i.getDescriptionShort(), times, t);
 		 }
 >>>>>>> removed finalize method and functionality to insert the neighbor set into the sample directly as it would result in wrong subgraphs
+=======
+		Graph g = nw3.generate();
+		g = t[0].transform(g);
+		g = t[1].transform(g);
+		
+		new GtnaGraphWriter().writeWithProperties(g, "./plots/network-plot/" + "base.txt");
+		for(int si = 0; si < 2; si++) {
+		    ((ExtractSampledSubgraph) t[2]).setIndex(si);
+		    Graph gi = t[2].transform(g);
+		    
+		    new GtnaGraphWriter().write(gi, "./plots/network-plot/" + "sample_" +si + ".txt");
+		    plotGraph(gi, "./plots/network-plot/", gi.toString() + "_"
+			+ si);
+		}	
+		 
+		
+		
+//		for(Network i : n){
+//			 System.out.println("Plotting network - " + i.getKey() + " @ " + i.getNodes() + " nodes");
+//			 plot(i, "./plots/network-plot/"+i.getDescriptionShort(), times, t);
+//		 }
+>>>>>>> fixes:
 	}
+	
+	
+	private static void plotGraph(Graph g, String dir, String filename) {
+
+		File d = new File(dir);
+		if (!d.exists() || !d.isDirectory()) {
+		    d.mkdirs();
+		}
+
+		filename = dir + filename;
+
+		Transformation t_rpid = new RandomPlaneIDSpaceSimple(1, 100, 100, true);
+		Transformation t_rrid = new RandomRingIDSpaceSimple(true);
+		Transformation t_crid = new ConsecutiveRingIDSpace(true);
+
+		Transformation t_nid = t_crid;
+
+		Gephi gephi = new Gephi();
+		if (t_nid == t_rpid) {
+		    Config.overwrite("GEPHI_RING_RADIUS", "1");
+		    Config.overwrite("GEPHI_NODE_BORDER_WIDTH", "0.01");
+		    Config.overwrite("GEPHI_EDGE_SCALE", "0.001");
+		    Config.overwrite("GEPHI_DRAW_CURVED_EDGES", "false");
+		    Config.overwrite("GEPHI_NODE_SIZE", "0.1");
+		} else if (t_nid == t_rrid || t_nid == t_crid) {
+		    Config.overwrite("GEPHI_RING_RADIUS", "50");
+		    Config.overwrite("GEPHI_NODE_BORDER_WIDTH", "0.01");
+		    Config.overwrite("GEPHI_EDGE_SCALE", "0.001");
+		    Config.overwrite("GEPHI_DRAW_CURVED_EDGES", "false");
+		    Config.overwrite("GEPHI_NODE_SIZE", "0.001");
+		}
+
+		g = t_nid.transform(g);
+
+		IdentifierSpace ids = (IdentifierSpace) g.getProperty("ID_SPACE_0");
+
+		// new GtnaGraphWriter().writeWithProperties(g, graphFilename+".txt");
+		// // writing is already done
+
+		gephi.plot(g, ids, filename + ".pdf");
+
+	    }
 	
 	
 	public static void plot(Network nw, String filename, int times, Transformation[] t) {
