af9e51172af0364c6aa82bff024ce7aaeb84ebd0
Tim Grube <grube@cs.tu-darmstadt.de>
2014-03-03 10:17:52 +0100

    prepare for real world networks metrics calculation

--- src/gtna/projects/sampling/CalculateMetrics.java
+++ src/gtna/projects/sampling/CalculateMetrics.java
@@ -76,9 +76,8 @@ public class CalculateMetrics {
 	private static ArrayList<Integer> scaledown = new ArrayList<Integer>();
 	private static String sampling = "samplingalgorithm";
 	private static String net;
-//	private static ArrayList<Integer> sizes = new ArrayList<Integer>();
+	// private static ArrayList<Integer> sizes = new ArrayList<Integer>();
 	private static String plotdir;
-
 
 	private static Set<Metric> metrics = new HashSet<Metric>();
 	private static int times;
@@ -89,56 +88,67 @@ public class CalculateMetrics {
 	 */
 	public static void main(String[] args) throws ParseException {
 
-			matchArguments(args);
+		matchArguments(args);
 
 		Collection<Network> rfc = new ArrayList<Network>();
 		for (int i = 0; i < dirs.size(); i++) {
 			// add scaled networks
-			for(int j = 0; j < scaledown.size(); j++){
-			String d = dirs.get(i);
+			for (int j = 0; j < scaledown.size(); j++) {
+				String d = dirs.get(i);
 
-			String p = d+net+"/"+scaledown.get(j)+"/"+sampling +"/";
-			File dir = new File(p);
-			File[] fs = dir.listFiles();
-			if (fs.length > 0) {
-				name = fs[0].getName().substring(0,
-						fs[0].getName().lastIndexOf("-"));
-			}
+				String p = d + net + "/" + scaledown.get(j) + "/" + sampling
+						+ "/";
+				
+				System.out.println("Reading directory: " + p);
+				File dir = new File(p);
+				File[] fs = dir.listFiles();
+				if (fs.length > 0) {
+					name = fs[0].getName().substring(0,
+							fs[0].getName().lastIndexOf("-"));
+				}
 
-//			System.out.println("Recognized name: " + name); // TODO REMOVE
+				// System.out.println("Recognized name: " + name); // TODO
+				// REMOVE
 
-			ReadableFolder rf = new ReadableFolder(name, name, p, suffix, null);
+				ReadableFolder rf = new ReadableFolder(name, name, p, suffix,
+						null);
 
-//			System.out.println("RF: " + d + " - size:" + rf.getFiles().length); // TODO
-																				// REMOVE
+				// System.out.println("RF: " + d + " - size:" +
+				// rf.getFiles().length); // TODO
+				// REMOVE
 
-			DescriptionWrapper dwrf = new DescriptionWrapper(rf, name+"-"+scaledown.get(j)+"%");
-			rfc.add(dwrf);
+				DescriptionWrapper dwrf = new DescriptionWrapper(rf, name + "-"
+						+ scaledown.get(j) + "%");
+				rfc.add(dwrf);
 			}
 			// add original network
 			String d = dirs.get(i);
-			
-			ReadableFile rf = new ReadableFile(net, net, d+net+suffix, null);
-			DescriptionWrapper dwrf = new DescriptionWrapper(rf, name+"-"+"100%");
+
+			ReadableFile rf = new ReadableFile(net, net, d + net + suffix, null);
+			DescriptionWrapper dwrf = new DescriptionWrapper(rf, name + "-"
+					+ "100%");
 			rfc.add(dwrf);
 		}
 
 		Network[] rfa = rfc.toArray(new Network[0]);
 
-		Config.overwrite("SKIP_EXISTING_DATA_FOLDERS", "true"); // TODO really? idk
-		
+		Config.overwrite("SKIP_EXISTING_DATA_FOLDERS", "true"); // TODO really?
+																// idk
+
 		Series[] series = new Series[rfa.length];
 		for (int i = 0; i < rfa.length; i++) {
 			String path;
-			if(i>=scaledown.size()){
-				path = targetdir + sampling + "/" + 100 + "/data";
+			if (i >= scaledown.size()) {
+				path = targetdir + "original" + "/" + 100 + "/data";
 			} else {
 				path = targetdir + sampling + "/" + scaledown.get(i) + "/data";
 			}
-//			System.out.println("Setting MAIN_DATA_FOLDER=" + path); // TODO remove
+			// System.out.println("Setting MAIN_DATA_FOLDER=" + path); // TODO
+			// remove
 			Config.overwrite("MAIN_DATA_FOLDER", path);
 
-			series[i] = Series.generate(rfa[i], metrics.toArray(new Metric[0]),	times);
+			series[i] = Series.generate(rfa[i], metrics.toArray(new Metric[0]),
+					times);
 
 			System.out.println(series[i].getFolder());
 			System.out.println(Arrays.toString(series[i].getRunFolders()));
@@ -158,21 +168,21 @@ public class CalculateMetrics {
 		Config.overwrite("TEMP_FOLDER", plotdir + "temp/");
 		Config.overwrite("RUNTIME_PLOTS_GENERATE", "false");
 		Config.overwrite("ETC_PLOTS_GENERATE", "false");
-		
+
 		Config.appendToList("GNUPLOT_CONFIG_1", "set logscale x");
 
-		Type plotType = Type.average; 			// Type.confidence1;
-		Style plotStyle = Style.linespoint; 	// Style.candlesticks;
-		
+		Type plotType = Type.average; // Type.confidence1;
+		Style plotStyle = Style.linespoint; // Style.candlesticks;
+
 		Plotting.single(series, metrics.toArray(new Metric[0]), "single/",
 				plotType, plotStyle); // main path to plots
-														// is set by
-														// Config.overwrite
+										// is set by
+										// Config.overwrite
 
 		Plotting.multi(series, metrics.toArray(new Metric[0]), "multi/",
 				plotType, plotStyle); // main path to plots
-														// is set by
-														// Config.overwrite
+										// is set by
+										// Config.overwrite
 
 	}
 
@@ -192,8 +202,8 @@ public class CalculateMetrics {
 				scaledown.addAll(matchScaledowns(s.substring(10)));
 			} else if (s.startsWith("times=")) {
 				times = Integer.parseInt(s.substring(6));
-//			}else if (s.startsWith("size=")) {
-//				sizes.add(Integer.parseInt(s.substring(5)));
+				// }else if (s.startsWith("size=")) {
+				// sizes.add(Integer.parseInt(s.substring(5)));
 			} else if (s.startsWith("metrics=")) {
 				metrics.addAll(matchMetrics(s.substring(8)));
 			} else if (s.startsWith("suffix=")) {
@@ -210,7 +220,7 @@ public class CalculateMetrics {
 				if (!f.isDirectory()) {
 					f.mkdir();
 				}
-			} 
+			}
 			// readable folder?
 			else if (s.startsWith("loaddir=")) {
 				dirs.add(s.substring(8));
@@ -227,12 +237,12 @@ public class CalculateMetrics {
 	 */
 	private static Collection<? extends Integer> matchScaledowns(
 			String substring) {
-		
+
 		LinkedList<Integer> sds = new LinkedList<Integer>();
-		for(String sd : substring.split(",")){
+		for (String sd : substring.split(",")) {
 			sds.offer(Integer.parseInt(sd));
 		}
-		
+
 		return sds;
 	}
 
@@ -242,24 +252,37 @@ public class CalculateMetrics {
 	 */
 	private static Collection<? extends Metric> matchMetrics(String s) {
 		LinkedList<Metric> recognized = new LinkedList<Metric>();
-		
-		for(String m : s.split(",")){
+
+		for (String m : s.split(",")) {
 			Metric i;
-			switch(m){
-				case "CC":i = new ClusteringCoefficient(); break;
-				case "DD":i = new DegreeDistribution(); break;
-				case "SP":i = new ShortestPaths(); break;
-				case "AS":i = new Assortativity(); break;
-				case "BC":i = new BetweennessCentrality(); break;
-				case "PR":i = new PageRank(); break;
-				default: i = null;
+			switch (m) {
+			case "CC":
+				i = new ClusteringCoefficient();
+				break;
+			case "DD":
+				i = new DegreeDistribution();
+				break;
+			case "SP":
+				i = new ShortestPaths();
+				break;
+			case "AS":
+				i = new Assortativity();
+				break;
+			case "BC":
+				i = new BetweennessCentrality();
+				break;
+			case "PR":
+				i = new PageRank();
+				break;
+			default:
+				i = null;
 			}
-			
-			if(i!=null){
+
+			if (i != null) {
 				recognized.offer(i);
 			}
 		}
-		
+
 		return recognized;
 	}
 }
