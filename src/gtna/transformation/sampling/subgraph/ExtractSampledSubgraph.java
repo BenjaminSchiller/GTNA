e988c4e614ba7bdefeb9a224681aaabb3a4bb13c
Tim Grube <grube@cs.tu-darmstadt.de>
2014-03-11 14:26:04 +0100

    reimplemented to extract the sampled subgraph faster

--- src/gtna/transformation/sampling/subgraph/ExtractSampledSubgraph.java
+++ src/gtna/transformation/sampling/subgraph/ExtractSampledSubgraph.java
@@ -46,9 +46,11 @@ import gtna.util.parameter.Parameter;
 import gtna.util.parameter.StringParameter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -79,13 +81,8 @@ public class ExtractSampledSubgraph extends Transformation {
 	@Override
 	public Graph transform(Graph g) {
 		
-		Timer tPrepare = new Timer();
 		Graph gi = new Graph(g.getName() + " (SAMPLED)");
-		
-		
-
 		Sample sample = (Sample) g.getProperty("SAMPLE_" + this.index);
-
 
 		HashSet<Integer> sampledIds = new HashSet<Integer>();
 		sampledIds.addAll(sample.getSampledIds()); // old Ids
@@ -94,72 +91,26 @@ public class ExtractSampledSubgraph extends Transformation {
 		Edges e = new Edges(ni, g.computeNumberOfEdges());
 
 		gi.setNodes(ni);
-		tPrepare.end();
 		
-		int c1 = 0;
-		int c2 = 0;
-		Timer tEdges2 = new Timer();
-		Iterator<Integer> sit = sampledIds.iterator();
-		boolean[] sampled = new boolean[g.getNodeCount()];
-		while(sit.hasNext()){
-			sampled[sit.next()]=true;
-		}
+		int[] map = new int[g.getNodeCount()];
+		Arrays.fill(map, -1);
 		
-		sit = sampledIds.iterator();
-		while(sit.hasNext()){
-			Node src = g.getNode(sit.next());
-			int srcNew = sample.getNewNodeId(src.getIndex());
-			for(int i: src.getOutgoingEdges()){
-				if(sampled[i]){
-					int dstNew = sample.getNewNodeId(i);
-					e.add(srcNew, dstNew);
-					c1++; // TODO 
+		for(Integer id : sampledIds){ // save at index i the new node index of node i
+			map[id] = sample.getNewNodeId(id);
+		}		
+		
+		Node[] nodes = g.getNodes();
+		for(Integer id : sampledIds){
+			Node src = nodes[id];
+			for(int dst : src.getOutgoingEdges()){
+				if(map[dst]>0){
+					e.add(map[id], map[dst]);
+
 				}
 			}
 		}
-		
-		tEdges2.end();
-		
-		
-		/* new */
-		
-		Timer tEdges3 = new Timer();
-		Iterator<Integer> sit1 = sampledIds.iterator();
-		boolean[] sampled1 = new boolean[g.getNodeCount()];
-		while(sit1.hasNext()){
-			sampled1[sit1.next()]=true;
-		}
-		
-		LinkedList<Edge> oldedges = new LinkedList<Edge>();
-		oldedges.addAll(g.getEdges().getEdges());
-		
-		java.util.ListIterator<Edge> lit = oldedges.listIterator();
-		
-		while(lit.hasNext()){
-			Edge c = lit.next();
-			if(!sampled1[c.getSrc()] || !sampled1[c.getDst()]){
-				lit.remove();
-			}
-		}
-		
-		while(lit.hasPrevious()){
-			Edge p = lit.previous();
-			e.add(sample.getNewNodeId(p.getSrc()), sample.getNewNodeId(p.getDst()));
-			c2++;
-		}
-		
-		tEdges3.end();
-		
-		
-		
-		
+				
 		e.fill();
-		
-		
-		System.out.println("subgraph: \t(0) Collecting Edges took "+  tEdges2.getSec() 
-				+ "s \n\t\t(1) Collecting Edges took "+ tEdges3.getSec() + "\n\t\tAdded " + c1 + "/" + c2 + "edges");
-		
-		gi.setName(g.getName() + " (SAMPLED)");
 
 		return gi;
 	}
